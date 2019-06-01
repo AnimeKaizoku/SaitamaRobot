f2269fa0dbe38637e8587c23921dbfaad237c55e
TsunayoshiSawada <34431649+TsunayoshiSawada@users.noreply.github.com>
2019-06-02 01:17:49 +0530

    Another minor translation update
    Ban response generalized

--- tg_bot/modules/bans.py
+++ tg_bot/modules/bans.py
@@ -41,7 +41,7 @@ def ban(bot: Bot, update: Update, args: List[str]) -> str:
             raise
 
     if is_user_ban_protected(chat, user_id, member):
-        message.reply_text("Why?")
+        message.reply_text("This user has immunity - I can't ban them")
         return ""
 
     if user_id == bot.id:
