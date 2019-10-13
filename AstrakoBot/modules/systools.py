34829a42f58665c99d663f2d838d253ff7c9a437
nunopenim <nuno.penim@gmail.com>
2019-10-13 09:41:20 +0100

    systools: cleanup of unused imports

--- AstrakoBot/modules/systools.py
+++ AstrakoBot/modules/systools.py
@@ -1,17 +1,10 @@
-from typing import Optional, List
-import requests
 import subprocess
 import os
-import sys
 
-from telegram import Message, Chat, Update, Bot, MessageEntity
-from telegram import ParseMode
+from telegram import Update, Bot
 from telegram.ext import CommandHandler, run_async, Filters
-from telegram.utils.helpers import escape_markdown, mention_html
-from tg_bot.modules.helper_funcs.extraction import extract_text
 
 from tg_bot import dispatcher, OWNER_ID, SUDO_USERS
-from tg_bot.modules.disable import DisableAbleCommandHandler
 from tg_bot.modules.helper_funcs.filters import CustomFilters
 
 @run_async
