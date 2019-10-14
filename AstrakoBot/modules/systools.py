f4ac4f86bb0bbfd820ab3d548c27db097fe89582
nunopenim <nuno.penim@gmail.com>
2019-10-14 12:52:57 +0100

    systools: added status command

--- AstrakoBot/modules/systools.py
+++ AstrakoBot/modules/systools.py
@@ -1,11 +1,41 @@
 import subprocess
 import os
 
-from telegram import Update, Bot
+from platform import python_version
+from telegram import Update, Bot, Message, Chat
 from telegram.ext import CommandHandler, run_async, Filters
 
 from tg_bot import dispatcher, OWNER_ID, SUDO_USERS
 from tg_bot.modules.helper_funcs.filters import CustomFilters
+from tg_bot.modules.disable import DisableAbleCommandHandler
+
+def pingme():
+    out = ""
+    under = False
+    if os.name == 'nt':
+        output = subprocess.check_output("ping -n 1 1.0.0.1 | findstr time*", shell=True).decode()
+        outS = output.splitlines()
+        out = outS[0]
+    else:
+        out = subprocess.check_output("ping -c 1 1.0.0.1 | grep time=", shell=True).decode()
+    splitOut = out.split(' ')
+    stringtocut = ""
+    for line in splitOut:
+        if(line.startswith('time=') or line.startswith('time<')):
+            stringtocut=line
+            break
+    newstra=stringtocut.split('=')
+    if len(newstra) == 1:
+        under = True
+        newstra=stringtocut.split('<')
+    newstr=""
+    if os.name == 'nt':
+        newstr=newstra[1].split('ms')
+    else:
+        newstr=newstra[1].split(' ') #redundant split, but to try and not break windows ping
+    ping_time = float(newstr[0])
+    return ping_time
+
 
 @run_async
 def poweroff(bot: Bot, update: Update):
@@ -15,7 +45,17 @@ def poweroff(bot: Bot, update: Update):
     subprocess.check_output('kill '+str(pid), shell=True)
     return
 
+@run_async
+def status(bot: Bot, update: Update):
+    pingSpeed = pingme()
+    reply = "System Status: operational\n\n"
+    reply += "Python version: "+python_version()+"\n"
+    reply += "Ping speed: "+str(pingSpeed)+"ms\n"
+    update.effective_message.reply_text(reply)
 
+
+STATUS_HANDLER = DisableAbleCommandHandler("status", status)
 SHUTDOWN_HANDLER = CommandHandler("shutdown", poweroff, filters=Filters.chat(OWNER_ID)) #safety...
 
 dispatcher.add_handler(SHUTDOWN_HANDLER)
+dispatcher.add_handler(STATUS_HANDLER)
