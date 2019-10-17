92c05ee53000692cea42681355e8d346cc0a1b1a
nunopenim <nuno.penim@gmail.com>
2019-10-17 17:57:53 +0100

    systools: shutdown now is *hopefully* universal

--- AstrakoBot/modules/systools.py
+++ AstrakoBot/modules/systools.py
@@ -40,9 +40,8 @@ def pingme():
 @run_async
 def poweroff(bot: Bot, update: Update):
     msg = update.effective_message
-    pid = os.getpid()
     msg.reply_text("Powering off")
-    subprocess.check_output('kill '+str(pid), shell=True)
+    os._exit(2)
     return
 
 @run_async
