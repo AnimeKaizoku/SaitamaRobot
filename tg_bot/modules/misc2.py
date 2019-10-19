d93a1f427890ffed5aa50a1b5caa85117890b9f2
Sawada Tsunayoshi <34431649+TsunayoshiSawada@users.noreply.github.com>
2019-10-19 20:24:51 +0530

    Added missing imports

--- tg_bot/modules/misc2.py
+++ tg_bot/modules/misc2.py
@@ -10,7 +10,7 @@ from telegram import Message, Chat, Update, Bot, MessageEntity
 from telegram import ParseMode
 from telegram.ext import CommandHandler, run_async, Filters
 from telegram.utils.helpers import escape_markdown, mention_html
-
+from tg_bot.modules.helper_funcs.chat_status import user_admin, sudo_plus
 from tg_bot import dispatcher, OWNER_ID, SUDO_USERS, SUPPORT_USERS, DEV_USERS, WHITELIST_USERS, BAN_STICKER
 from tg_bot.__main__ import STATS, USER_INFO
 from tg_bot.modules.disable import DisableAbleCommandHandler, DisableAbleRegexHandler
