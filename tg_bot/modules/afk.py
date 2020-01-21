240a502917340c58a240aeafa36bb710278dda69
Sawada Tsunayoshi <34431649+TsunayoshiSawada@users.noreply.github.com>
2020-01-21 17:42:06 +0530

    Improved translation and replies for AFK

--- tg_bot/modules/afk.py
+++ tg_bot/modules/afk.py
@@ -22,7 +22,7 @@ def afk(bot: Bot, update: Update):
         reason = ""
 
     sql.set_afk(update.effective_user.id, reason)
-    update.effective_message.reply_text("{} is now AFK!".format(update.effective_user.first_name))
+    update.effective_message.reply_text("{} is now away!".format(update.effective_user.first_name))
 
 
 @run_async
@@ -34,8 +34,18 @@ def no_longer_afk(bot: Bot, update: Update):
 
     res = sql.rm_afk(user.id)
     if res:
-        update.effective_message.reply_text("{} is no longer AFK!".format(update.effective_user.first_name))
-
+       options = [
+                    '{} is here!',
+                    '{} is back!',
+                    '{} is now in the chat!',
+                    '{} is awake!',
+                    '{} is back online!',
+                    '{} is finally here!',
+                    'Welcome back!, {}',
+                    'Where is {}?\nIn the chat!',
+                  ]
+       chosen_option = random.choice(options)
+       update.effective_message.reply_text(chosen_option.format(update.effective_user.first_name))
 
 @run_async
 def reply_afk(bot: Bot, update: Update):
@@ -64,7 +74,7 @@ def reply_afk(bot: Bot, update: Update):
                     if not reason:
                         res = "{} is AFK!".format(fst_name)
                     else:
-                        res = "{} is AFK! says its because of:\n{}".format(fst_name, reason)
+                        res = "{} is AFK!\nReason:\n{}".format(fst_name, reason)
                     message.reply_text(res)
 
 
@@ -73,7 +83,7 @@ def __gdpr__(user_id):
 
 
 __help__ = """
- - /afk <reason>: mark yourself as AFK.
+ - /afk <reason>: mark yourself as AFK(away from keyboard).
  - brb <reason>: same as the afk command - but not a command.
 When marked as AFK, any mentions will be replied to with a message to say you're not available!
 """
