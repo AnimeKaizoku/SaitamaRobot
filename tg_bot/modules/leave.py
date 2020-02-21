069a63d8283bccd1f55c761fbe18ce3628d4291c
Loli-Killer <smartdavid.2001@gmail.com>
2020-02-21 20:37:01 +0630

    update leave.py

--- tg_bot/modules/leave.py
+++ tg_bot/modules/leave.py
@@ -1,30 +1,22 @@
-from telegram import TelegramError, Chat, Message
-from telegram import Update, Bot
-from telegram.error import BadRequest
-from telegram.ext import MessageHandler, Filters, CommandHandler
-from telegram.ext.dispatcher import run_async
-from typing import List
-from tg_bot.modules.helper_funcs.filters import CustomFilters
+from telegram import Bot, TelegramError, Update
+from telegram.ext import CommandHandler, Filters, run_async
 
-import telegram
 from tg_bot import dispatcher, DEV_USERS
 
 @run_async
 def leave(bot: Bot, update: Update, args: List[str]):
+
     if args:
         chat_id = str(args[0])
-        del args[0]
         try:
             bot.leave_chat(int(chat_id))
-            update.effective_message.reply_text("Done. ")
-        except telegram.TelegramError:
+            update.effective_message.reply_text("Done.")
+        except TelegramError:
             update.effective_message.reply_text("Couldnt leave group.")
     else:
         update.effective_message.reply_text("Send a valid chat id") 
 
-__help__ = ""
-
-__mod_name__ = "Leave"
-
 LEAVE_HANDLER = CommandHandler("leave", leave, pass_args = True, filters=Filters.user(DEV_USERS))
 dispatcher.add_handler(LEAVE_HANDLER)
+
+__mod_name__ = "Leave"
