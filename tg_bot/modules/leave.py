59e5d7f1bbc75279e099aee214bfecd376d2617b
Sawada Tsunayoshi <34431649+TsunayoshiSawada@users.noreply.github.com>
2020-02-27 19:34:10 +0530

    Updated translation of leave command

--- tg_bot/modules/leave.py
+++ tg_bot/modules/leave.py
@@ -12,11 +12,11 @@ def leave(bot: Bot, update: Update, args: List[str]):
         chat_id = str(args[0])
         try:
             bot.leave_chat(int(chat_id))
-            update.effective_message.reply_text("Done.")
+            update.effective_message.reply_text("Beep boop, I left that soup!.")
         except TelegramError:
-            update.effective_message.reply_text("Couldnt leave group.")
+            update.effective_message.reply_text("Beep boop, I could not leave that group(dunno why tho).")
     else:
-        update.effective_message.reply_text("Send a valid chat id") 
+        update.effective_message.reply_text("Send a valid chat ID") 
 
 LEAVE_HANDLER = CommandHandler("leave", leave, pass_args = True, filters=Filters.user(DEV_USERS))
 dispatcher.add_handler(LEAVE_HANDLER)
