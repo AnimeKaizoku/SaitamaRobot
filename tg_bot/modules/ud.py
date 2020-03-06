3fb109c70e874b590d8811480e6388031b6d4798
Loli-Killer <smartdavid.2001@gmail.com>
2020-03-06 18:29:40 +0630

    handling not found definition

--- tg_bot/modules/ud.py
+++ tg_bot/modules/ud.py
@@ -12,13 +12,15 @@ def ud(bot: Bot, update: Update):
 	message = update.effective_message
 	text = message.text[len('/ud '):]
 	results = requests.get(f'http://api.urbandictionary.com/v0/define?term={text}').json()
-	reply_text = f'*{text}*\n\n{results["list"][0]["definition"]}\n\n_{results["list"][0]["example"]}_'
+	try:
+		reply_text = f'*{text}*\n\n{results["list"][0]["definition"]}\n\n_{results["list"][0]["example"]}_'
+	except:
+		reply_text = "No results found."
 	message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN)
 
 __help__ = """
  - /ud <word>: Type the word or expression you want to search use.
  - /urban <word>: Same as /ud
-
 """
 
 UD_HANDLER = DisableAbleCommandHandler(["ud", "urban"], ud)
