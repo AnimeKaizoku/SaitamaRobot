1f63097d7a4300aa722039c9373a11e6e36adfc8
Sawada Tsunayoshi <34431649+TsunayoshiSawada@users.noreply.github.com>
2020-03-06 14:38:19 +0530

    Commented out replies for admins for auto warns
    Ittu pittu jhimm patuta

--- tg_bot/modules/warns.py
+++ tg_bot/modules/warns.py
@@ -27,7 +27,7 @@ CURRENT_WARNING_FILTER_STRING = "<b>Current warning filters in this chat:</b>\n"
 # Not async
 def warn(user: User, chat: Chat, reason: str, message: Message, warner: User = None) -> str:
     if is_user_admin(chat, user.id):
-        message.reply_text("Damn admins, They are too far to be One Punched!")
+       # message.reply_text("Damn admins, They are too far to be One Punched!")
         return ""
 
     if warner:
