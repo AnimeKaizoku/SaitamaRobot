67511124a4bffc1a8e698f5d556e47e0676f59d6
nunopenim <nuno.penim@gmail.com>
2020-03-11 13:44:48 +0000

    github: updated help

--- AstrakoBot/modules/github.py
+++ AstrakoBot/modules/github.py
@@ -157,13 +157,14 @@ __help__ = """
 Github module. This module will fetch github releases.
 Commands:
  - /git <user>/<repo>: will fetch the most recent release from that repo.
+ - /git <user>/<repo> <number>: will fetch releases in past.
  - /fetch <reponame> or &reponame: same as /git, but you can use a saved repo shortcut
  - /listrepo: lists all repo shortcuts in chat
  - /gitver: returns the current API version
  - /changelog <reponame>: gets the changelog of a saved repo shortcut
  
 Admin only:
- - /saverepo <name> <user>/<repo>: saves a repo value as shortcut
+ - /saverepo <name> <user>/<repo> <number (optional)>: saves a repo value as shortcut
  - /delrepo <name>: deletes a repo shortcut
 """
 
