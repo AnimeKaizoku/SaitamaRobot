af504e001c559727c76107f8bf3295d5077517f0
Sawada Tsunayoshi <34431649+TsunayoshiSawada@users.noreply.github.com>
2020-03-20 14:17:15 +0530

    Change flood ban to mute
    Note: I'm not adept in messing around functions like this, this is merely a simple edit and is possibly unstable or unreliable. 
    A review is requested by someone who knows this better.

--- tg_bot/modules/antiflood.py
+++ tg_bot/modules/antiflood.py
@@ -35,12 +35,12 @@ def check_flood(bot: Bot, update: Update) -> str:
         return log_message
 
     try:
-        chat.kick_member(user.id)
-        msg.reply_text("*bans user*\nReason: Flood")
+        bot.restrict_chat_member(chat.id, user.id, can_send_messages=False)
+        msg.reply_text("*permamutes user*\nReason: Flood")
         log_message = (f"<b>{html.escape(chat.title)}:</b>\n"
-                       f"#BANNED\n"
+                       f"#MUTED\n"
                        f"<b>User:</b> {mention_html(user.id, user.first_name)}\n"
-                       f"Flooded the group.")
+                       f"Flooded the group. Muted until an admin unmutes")
 
         return log_message
 
