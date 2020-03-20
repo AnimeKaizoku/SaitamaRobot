5bea86475783c87a940f66161097b3b88f0d1bbd
DragSama <49992845+DragSama@users.noreply.github.com>
2020-03-20 23:20:35 +0530

    removed speedtest and renamed ex.py to paste.py

--- tg_bot/modules/paste.py
+++ tg_bot/modules/paste.py
@@ -1,7 +1,6 @@
 from typing import List
 
 import requests
-import speedtest
 from telegram import Update, Bot, ParseMode
 from telegram.ext import run_async
 
@@ -32,19 +31,5 @@ def paste(bot: Bot, update: Update, args: List[str]):
     message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
 
 
-@dev_plus
-@run_async
-def speedtestxyz(bot: Bot, update: Update):
-    s = speedtest.Speedtest()
-    msg = update.effective_message.reply_text("Doing SpeedTest.... ")
-    s.get_best_server()
-    s.download()
-    s.upload()
-    speedtest_image = s.results.share()
-    update.effective_message.reply_photo(photo=speedtest_image, caption='Done!')
-    msg.delete()
-
 PASTE_HANDLER = DisableAbleCommandHandler("paste", paste, pass_args=True)
-SpeedTest_handler = DisableAbleCommandHandler("speedtest", speedtestxyz)
-dispatcher.add_handler(SpeedTest_handler)
 dispatcher.add_handler(PASTE_HANDLER)
