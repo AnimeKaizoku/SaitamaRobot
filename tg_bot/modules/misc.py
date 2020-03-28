f1a2af31f3542ab8b5efea35377160fec2da98a5
DragSama <49992845+DragSama@users.noreply.github.com>
2020-03-28 22:13:45 +0530

    Updated "?" in info

--- tg_bot/modules/misc.py
+++ tg_bot/modules/misc.py
@@ -146,7 +146,7 @@ def info(bot: Bot, update: Update, args: List[str]):
         disaster_level_present = True
 
     if disaster_level_present:
-        text += ' [<a href="https://t.me/OnePunchSupport/18340">?</a>]'
+        text += ' [<a href="http://t.me/SaitamaShikenRobot?start=disasters">?</a>]'
 
     text += "\n"
     for mod in USER_INFO:
