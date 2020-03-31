d58dc3874b7aa10cad362e7c72cca74d02ca04d8
Sawada Tsunayoshi <34431649+TsunayoshiSawada@users.noreply.github.com>
2020-03-31 16:35:37 +0530

    never push all commits in one go tbh
    NEVER

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -74,6 +74,8 @@ if ENV:
     ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
     CASH_API_KEY = os.environ.get('CASH_API_KEY', None)
     TIME_API_KEY = os.environ.get('TIME_API_KEY', None)
+    AI_API_KEY = os.environ.get('AI_API_KEY', "")
+
 
 else:
     from tg_bot.config import Development as Config
