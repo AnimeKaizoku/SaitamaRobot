655494e8d9e4f6024d25c82925534a20ae166aa2
Sawada Tsunayoshi <34431649+TsunayoshiSawada@users.noreply.github.com>
2020-03-31 16:59:17 +0530

    minor edit to apikey line

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -74,7 +74,7 @@ if ENV:
     ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
     CASH_API_KEY = os.environ.get('CASH_API_KEY', None)
     TIME_API_KEY = os.environ.get('TIME_API_KEY', None)
-    AI_API_KEY = os.environ.get('AI_API_KEY', "")
+    AI_API_KEY = os.environ.get('AI_API_KEY', None)
 
 
 else:
