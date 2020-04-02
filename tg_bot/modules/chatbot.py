8043808c51c57b19b489fb28a312745f7c89845c
Sawada Tsunayoshi <34431649+TsunayoshiSawada@users.noreply.github.com>
2020-04-02 22:44:17 +0530

    Updated help a bit

--- tg_bot/modules/chatbot.py
+++ tg_bot/modules/chatbot.py
@@ -90,7 +90,7 @@ def chatbot(bot: Bot, update: Update):
 __mod_name__ = "Chatbot"
 
 __help__ = """
-Chatbot utilizes CoffeeHouse API and allows Saitama to chat with your users making your chat more interactive.
+Chatbot utilizes the CoffeeHouse API and allows Saitama to talk back making your chat more interactive.
 This is an ongoing upgrade and is only available in your chats if you reach out to @OnePunchSupport and ask for it. 
 
 In future we might make it open for any chat and controllable by group admins.
