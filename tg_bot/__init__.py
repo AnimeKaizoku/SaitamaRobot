444be7e195fc5e22dd0b95dcf0626fd646cd7f95
Sawada Tsunayoshi <34431649+TsunayoshiSawada@users.noreply.github.com>
2020-04-02 22:31:35 +0530

    Added missing lines desu

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -133,6 +133,8 @@ else:
     ALLOW_EXCL = Config.ALLOW_EXCL
     CASH_API_KEY = Config.CASH_API_KEY
     TIME_API_KEY = Config.TIME_API_KEY
+    AI_API_KEY = Config.AI_API_KEY
+    WALL_API = Config.WALL_API    
 
 SUDO_USERS.add(OWNER_ID)
 DEV_USERS.add(OWNER_ID)
