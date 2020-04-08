155b2db898d7b989dd8630df7441e711f2e82bb0
Sawada Tsunayoshi <34431649+TsunayoshiSawada@users.noreply.github.com>
2020-04-08 17:19:09 +0530

    adawdawdawdawdawdawdawd

--- tg_bot/modules/sql/afk_sql.py
+++ tg_bot/modules/sql/afk_sql.py
@@ -32,10 +32,9 @@ def is_afk(user_id):
 
 
 def check_afk_status(user_id):
-    try:
-        return SESSION.query(AFK).get(user_id)
-    finally:
-        SESSION.close()
+    if user_id in AFK_USERS:
+        return True, AFK_USERS[user_id]
+    return False, ""
 
 
 def set_afk(user_id, reason=""):
@@ -45,6 +44,7 @@ def set_afk(user_id, reason=""):
             curr = AFK(user_id, reason, True)
         else:
             curr.is_afk = True
+            curr.reason = reason
 
         AFK_USERS[user_id] = reason
 
@@ -67,27 +67,11 @@ def rm_afk(user_id):
         return False
 
 
-def toggle_afk(user_id, reason=""):
-    with INSERTION_LOCK:
-        curr = SESSION.query(AFK).get(user_id)
-        if not curr:
-            curr = AFK(user_id, reason, True)
-        elif curr.is_afk:
-            curr.is_afk = False
-        elif not curr.is_afk:
-            curr.is_afk = True
-        SESSION.add(curr)
-        SESSION.commit()
-
-
 def __load_afk_users():
     global AFK_USERS
     try:
         all_afk = SESSION.query(AFK).all()
-        AFK_USERS = {
-            user.user_id: user.reason
-            for user in all_afk if user.is_afk
-        }
+        AFK_USERS = {user.user_id: user.reason for user in all_afk if user.is_afk}
     finally:
         SESSION.close()
 
