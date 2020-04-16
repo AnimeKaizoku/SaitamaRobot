1f3ea084e3e7a5f23b7868ef7f16255f9bf3b8ea
amalmadev <57911733+amalmadev@users.noreply.github.com>
2020-04-16 10:30:42 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -29,7 +29,7 @@ The support group chat is at @OnePunchSupport
 
 HELP_STRINGS = """
 Hey there! My name is *{}*.
-I'm a Hero For Fun and help admins manage their groups with One Punch! Have a look at the following for an idea of some of \
+I'm a Hero For Fun and help admins  manage their groups with One Punch! Have a look at the following for an idea of some of \
 the things I can help you with.
 
 *Main* commands available:
