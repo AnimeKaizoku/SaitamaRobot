f3958c333fa9f16f92a214539bf7fb3737857c5a
DragSama <49992845+DragSama@users.noreply.github.com>
2020-05-07 23:17:44 +0530

    Use regex for filters.

--- tg_bot/modules/cust_filters.py
+++ tg_bot/modules/cust_filters.py
@@ -169,7 +169,7 @@ def reply_filter(bot: Bot, update: Update):
 
     chat_filters = sql.get_chat_triggers(chat.id)
     for keyword in chat_filters:
-        pattern = r"( |^|[^\w])" + re.escape(keyword) + r"( |$|[^\w])"
+        pattern = r"( |^|[^\w])" + keyword + r"( |$|[^\w])"
         if re.search(pattern, to_match, flags=re.IGNORECASE):
             filt = sql.get_filter(chat.id, keyword)
             if filt.is_sticker:
