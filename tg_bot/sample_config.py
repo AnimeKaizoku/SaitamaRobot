5ca80a9e853883c25d71d0c9a8b20ab557c4a92b
animekaizoku <animekaizokux@gmail.com>
2020-05-12 23:32:16 +0530

    Noob 100, pushed shiken change of sample readme update to master

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -37,7 +37,6 @@ class Config(object):
     PORT = 5000
     DEL_CMDS = True  #Delete commands that users dont have access to, like delete /ban if a non admin uses it.
     STRICT_GBAN = True
-    STRICT_GMUTE = False
     WORKERS = 8  # Number of subthreads to use. Set as number of threads your processor uses
     BAN_STICKER = 'CAADAgADOwADPPEcAXkko5EB3YGYAg'  # banhammer marie sticker
     ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
