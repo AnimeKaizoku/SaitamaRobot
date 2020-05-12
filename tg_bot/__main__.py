5bc6244e3496716d883c0baddcf95009cffa0e20
DragSama <49992845+DragSama@users.noreply.github.com>
2020-05-12 13:24:01 +0530

    Ignore pending updates

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -441,7 +441,7 @@ def main():
 
     else:
         LOGGER.info("Using long polling.")
-        updater.start_polling(timeout=15, read_latency=4)
+        updater.start_polling(timeout=15, read_latency=4, clean=True)
 
     updater.idle()
 
