5277c1cda9aa3af12e48b7785cbf06e4a919e8fe
DragSama <49992845+DragSama@users.noreply.github.com>
2020-05-15 15:40:20 +0530

    a new file owo

---
+++ tg_bot/modules/helper_funcs/regex_helper.py
@@ -0,0 +1,20 @@
+import timeout_decorator
+import re
+
+#Not using signals because Saitama is hosted on Windows
+@timeout_decorator.timeout(5, use_signals=False) 
+def regex_searcher(regex, string):
+   try:
+     search = re.search(regex, string)
+   except timeout_decorator.timeout_decorator.TimeoutError:
+     return 'Timeout'
+   except Exception as e:
+     return E
+   return search
+
+def infinite_loop_check(regex):
+     loop_matches = [r'\((.{1,}[\+\*]){1,}\)[\+\*].', r'[\(\[].{1,}\{\d(,)?\}[\)\]]\{\d(,)?\}', r'\(.{1,}\)\{.{1,}(,)?\}\(.*\)(\+|\* |\{.*\})']
+     for match in loop_matches:
+          match_1 = re.search(match, regex)
+          if match_1: return True
+     return False
