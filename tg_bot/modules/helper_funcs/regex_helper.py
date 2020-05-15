7382c78e3dd9a577521947924f08ca29569e6959
DragSama <49992845+DragSama@users.noreply.github.com>
2020-05-15 15:47:00 +0530

    raise instead of return

--- tg_bot/modules/helper_funcs/regex_helper.py
+++ tg_bot/modules/helper_funcs/regex_helper.py
@@ -9,7 +9,7 @@ def regex_searcher(regex, string):
    except timeout_decorator.timeout_decorator.TimeoutError:
      return 'Timeout'
    except Exception as e:
-     return e
+     raise e
    return search
 
 def infinite_loop_check(regex):
