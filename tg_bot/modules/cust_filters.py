182c514298ffbd1624eddce074e0a93c7f0a82dc
DragSama <49992845+DragSama@users.noreply.github.com>
2020-05-15 16:37:46 +0530

    fixed error

--- tg_bot/modules/cust_filters.py
+++ tg_bot/modules/cust_filters.py
@@ -180,7 +180,7 @@ def reply_filter(bot: Bot, update: Update):
            reason, error = match, True
         if error:
            sql.remove_filter(chat.id, keyword)
-           msg.reply_text(f'Removed {trigger} from blacklist because of {reason}')
+           message.reply_text(f'Removed {trigger} from blacklist because of {reason}')
            return
         
         if match:
