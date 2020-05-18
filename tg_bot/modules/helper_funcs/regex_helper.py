5038cb8995ab4642fe9718e4dfa7604a4c703a9b
DragSama <49992845+DragSama@users.noreply.github.com>
2020-05-18 22:57:02 +0530

    use regex instead of re

--- tg_bot/modules/helper_funcs/regex_helper.py
+++ tg_bot/modules/helper_funcs/regex_helper.py
@@ -1,28 +1,18 @@
-import timeout_decorator
-import re
-
-
-#Not using signals because Saitama is hosted on Windows
-@timeout_decorator.timeout(15, use_signals=False) 
-def regex_runner(regex, string):
-   search = re.search(regex, string)
-   return search
+import regex
 
  
 def regex_searcher(regex, string):
    try:
-     search = regex_runner(regex, string)
-   except timeout_decorator.timeout_decorator.TimeoutError as t:
-     print(t)
-     return 'Timeout'
-   except Exception as e:
-      print(e)
-      return 'Broken'
+     search = regex.search(regex, string, timeout= 6)
+   except TimeoutError:
+     return False
+   except Exception:
+      return False
    return search
 
 def infinite_loop_check(regex):
      loop_matches = [r'\((.{1,}[\+\*]){1,}\)[\+\*].', r'[\(\[].{1,}\{\d(,)?\}[\)\]]\{\d(,)?\}', r'\(.{1,}\)\{.{1,}(,)?\}\(.*\)(\+|\* |\{.*\})']
      for match in loop_matches:
-          match_1 = re.search(match, regex)
+          match_1 = regex.search(match, regex)
           if match_1: return True
      return False
