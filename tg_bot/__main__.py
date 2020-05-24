6621de00bc66c2ce5f69215a1233a3e6382ac290
Thor Odinson <63148306+thor-odinson-17@users.noreply.github.com>
2020-05-24 21:15:41 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -22,7 +22,7 @@ I am an Anime themed group management bot.
 To add me to your group click ["HERE"](t.me/shield_agent_bot?startgroup=botstart)
 You can find my list of available commands with /help.
 
-I was made with Love by [tansique](t.e/tansique_17).
+I was made with Love by [Tansique](https://t.me/tansique_17).
 """
 
 HELP_STRINGS = """
