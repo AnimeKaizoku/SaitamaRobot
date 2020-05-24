636c6a1ebfbe6406f612376612e1d1c965b963ac
Ritvik Mishra <51820183+Jade9ja@users.noreply.github.com>
2020-05-25 01:08:26 +0530

    Fixed possible typos (?)

--- tg_bot/modules/disasters.py
+++ tg_bot/modules/disasters.py
@@ -32,9 +32,9 @@ disasters = """ Saitama has bot access levels we call as *"Disaster Levels"*
 \n*God* - Only one exists, bot owner. 
 Owner has complete bot access, including bot adminship in chats Saitama is at.
 \n*Dragons* - Have super user access, can gban, manage disasters lower than them and are admins in Saitama.
-\n*Demons* - Have access go globally ban users across Saitama.
+\n*Demons* - Have access to globally ban users across Saitama.
 \n*Tigers* - Same as wolves but can unban themselves if banned.
-\n*Wolves* - Cannot be banned, muted flood kicked but can be manually banned by admins.
+\n*Wolves* - Cannot be banned, muted or flood kicked but can be manually banned by admins.
 \n*Disclaimer*: The disaster levels in Saitama are there for troubleshooting, support, banning potential scammers.
 Report abuse or ask us more on these at [Heroes Association](https://t.me/OnePunchSupport).
 """
