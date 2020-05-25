b766afbaf8fcf948936446e63c0d7a22ffcb1503
animekaizoku <animekaizokux@gmail.com>
2020-05-25 22:07:47 +0530

    [Admin.py] Added missing SUDO_USERS import

--- tg_bot/modules/admin.py
+++ tg_bot/modules/admin.py
@@ -7,7 +7,7 @@ from telegram.error import BadRequest
 from telegram.ext import CommandHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 
-from tg_bot import dispatcher, TOKEN
+from tg_bot import dispatcher, TOKEN, SUDO_USERS
 from tg_bot.modules.disable import DisableAbleCommandHandler
 from tg_bot.modules.helper_funcs.chat_status import bot_admin, can_promote, user_admin, can_pin, connection_status
 from tg_bot.modules.helper_funcs.extraction import extract_user, extract_user_and_text
