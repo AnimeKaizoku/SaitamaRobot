e99e758d0611592173d9f085b79cd4e7a414b55f
starry69 <starry369126@outlook.com>
2020-07-22 17:48:20 +0530

    fix typo
    Signed-off-by: starry69 <starry369126@outlook.com>

--- SaitamaRobot/modules/stickers.py
+++ SaitamaRobot/modules/stickers.py
@@ -36,18 +36,19 @@ def stickerid(update: Update, context: CallbackContext):
 
 @run_async
 def getsticker(update: Update, context: CallbackContext):
-  bot = context.bot
-  msg = update.effective_message
-  chat_id = update.effective_chat.id
-  if msg.reply_to_message and msg.reply_to_message.sticker:
-    file_id = msg.reply_to_message.sticker.file_id
-    new_file = bot.get_file(file_id)
-    new_file.download('sticker.png')
-    bot.send_document(chat_id, document=open('sticker.png', 'rb'))
-    os.remove("sticker.png")
-  else:
-    update.effective_message.reply_text(
-        "Please reply to a sticker for me to upload its PNG.")
+    bot = context.bot
+    msg = update.effective_message
+    chat_id = update.effective_chat.id
+    if msg.reply_to_message and msg.reply_to_message.sticker:
+        file_id = msg.reply_to_message.sticker.file_id
+        new_file = bot.get_file(file_id)
+        new_file.download("sticker.png")
+        bot.send_document(chat_id, document=open("sticker.png", "rb"))
+        os.remove("sticker.png")
+    else:
+        update.effective_message.reply_text(
+            "Please reply to a sticker for me to upload its PNG."
+        )
 
 
 @run_async
@@ -419,7 +420,6 @@ def makepack_internal(
         )
     else:
         msg.reply_text("Failed to create sticker pack. Possibly due to blek mejik.")
-
 
 
 __help__ = """
