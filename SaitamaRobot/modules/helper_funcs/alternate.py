a59230471e011168102c613abb6ce61d463fc6cd
starry69 <starry369126@outlook.com>
2020-07-30 21:33:10 +0530

    [Alternate.py] Added typing action

--- SaitamaRobot/modules/helper_funcs/alternate.py
+++ SaitamaRobot/modules/helper_funcs/alternate.py
@@ -7,3 +7,15 @@ def send_message(message, text, *args, **kwargs):
     except BadRequest as err:
         if str(err) == "Reply message not found":
             return message.reply_text(text, quote=False, *args, **kwargs)
+
+def typing_action(func):
+    """Sends typing action while processing func command."""
+
+    @wraps(func)
+    def command_func(update, context, *args, **kwargs):
+        context.bot.send_chat_action(
+            chat_id=update.effective_chat.id, action=ChatAction.TYPING
+        )
+        return func(update, context, *args, **kwargs)
+
+    return command_func
