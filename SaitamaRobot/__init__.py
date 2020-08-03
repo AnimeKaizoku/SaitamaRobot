d8b6e9e62f1257539f58972b5582cdcebfe43ca5
Sawada Tsunayoshi <34431649+TsunayoshiSawada@users.noreply.github.com>
2020-08-04 02:05:15 +0530

    Set log level back to INFO

--- SaitamaRobot/__init__.py
+++ SaitamaRobot/__init__.py
@@ -14,7 +14,7 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[logging.FileHandler('log.txt'),
               logging.StreamHandler()],
-    level=logging.DEBUG)
+    level=logging.INFO)
 
 LOGGER = logging.getLogger(__name__)
 
