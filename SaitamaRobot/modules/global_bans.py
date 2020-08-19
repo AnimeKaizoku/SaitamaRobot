cb769cecdc7ae98bf2ad1825fb420ff5240e3d36
starry69 <starry369126@outlook.com>
2020-08-19 11:06:52 +0530

    Fix one smol err in gban
    Signed-off-by: starry69 <starry369126@outlook.com>

--- SaitamaRobot/modules/global_bans.py
+++ SaitamaRobot/modules/global_bans.py
@@ -385,14 +385,14 @@ def check_and_ban(update, user_id, should_message=True):
 
     if sw_ban:
         update.effective_chat.kick_member(user_id)
-    if should_message:
-        update.effective_message.reply_text(
-            f"<b>Alert</b>: this user is globally banned.\n"
-            f"<code>*bans them from here*</code>.\n"
-            f"<b>Appeal chat</b>: {SPAMWATCH_SUPPORT_CHAT}\n"
-            f"<b>User ID</b>: <code>{sw_ban['id']}</code>\n"
-            f"<b>Ban Reason</b>: <code>{html.escape(sw_ban['reason'])}</code>",
-            parse_mode=ParseMode.HTML)
+        if should_message:
+            update.effective_message.reply_text(
+                 f"<b>Alert</b>: this user is globally banned.\n"
+                 f"<code>*bans them from here*</code>.\n"
+                 f"<b>Appeal chat</b>: {SPAMWATCH_SUPPORT_CHAT}\n"
+                 f"<b>User ID</b>: <code>{sw_ban['id']}</code>\n"
+                 f"<b>Ban Reason</b>: <code>{html.escape(sw_ban['reason'])}</code>",
+                 parse_mode=ParseMode.HTML)
         return
 
     if sql.is_user_gbanned(user_id):
