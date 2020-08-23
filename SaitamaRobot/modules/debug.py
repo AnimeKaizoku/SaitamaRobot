b953cd3f669997bcb829d38e3a87fc76d53bf11e
Loli-Killer <smartdavid.2001@gmail.com>
2020-08-23 19:40:54 +0630

    [Debug] Fixes

--- SaitamaRobot/modules/debug.py
+++ SaitamaRobot/modules/debug.py
@@ -10,8 +10,30 @@ from SaitamaRobot.modules.helper_funcs.chat_status import dev_plus
 
 DEBUG_MODE = False
 
+@run_async
+@dev_plus
+def debug(update: Update, context: CallbackContext):
+    global DEBUG_MODE
+    args = update.effective_message.text.split(None, 1)
+    message = update.effective_message
+    print(DEBUG_MODE)
+    if len(args) > 1:
+        if args[1] in ('yes', 'on'):
+            DEBUG_MODE = True
+            message.reply_text("Debug mode is now on.")
+        elif args[1] in ('no', 'off'):
+            DEBUG_MODE = False
+            message.reply_text("Debug mode is now off.")
+    else:
+        if DEBUG_MODE:
+            message.reply_text("Debug mode is currently on.")
+        else:
+            message.reply_text("Debug mode is currently off.")
+
+
 @telethn.on(events.NewMessage(pattern="[/!].*"))
 async def i_do_nothing_yes(event):
+    global DEBUG_MODE
     if DEBUG_MODE:
         if os.path.exists('updates.txt'):
             with open('updates.txt', 'r') as f:
@@ -21,23 +43,6 @@ async def i_do_nothing_yes(event):
         else:
             with open('updates.txt', 'w+') as f:
                 f.write(f"- {event.from_id} ({event.chat_id}) : {event.text} | {datetime.datetime.now()}")
-
-@run_async
-@dev_plus
-def debug(update: Update, context: CallbackContext):
-    args = update.effective_message.text.split(None, 1)
-    message = update.effective_message
-
-    if len(args) > 1:
-        if args[1] in ('yes', 'on'):
-            DEBUG_MODE = True
-        elif args[1] in ('no', 'off'):
-            DEBUG_MODE = False 
-    else:
-        if DEBUG_MODE:
-            message.reply_text("Debug mode is currently off.")
-        else:
-            message.reply_text("Debug mode is currently on.")
 
 DEBUG_HANDLER = CommandHandler("debug", debug)
 dispatcher.add_handler(DEBUG_HANDLER)
