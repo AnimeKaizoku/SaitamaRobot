7f8bba818c4388d1b2490261800d7517b1492ef0
Sawada Tsunayoshi <animekaizokux@gmail.com>
2020-08-23 12:05:18 +0530

    [batch] Comments and minor edits for batch files

--- SetupVirtualEnv_run_once.bat
+++ SetupVirtualEnv_run_once.bat
@@ -1,2 +1,6 @@
 TITLE Setting up virtual env
+:: Running it once is fine, this just sets up virtual env >> install all modules there 
 py -m venv env && env\scripts\activate.bat && pip install -r requirements.txt
+
+:: Note to rerun the requirements.txt in case you ever add a mdoule.
+:: Running this multiple time will not make a mess of your setup, dont worry about that bit.
--- start.bat
+++ start.bat
@@ -1,3 +1,4 @@
 @echo off
 TITLE Saitama Robot
+:: Enables virtual env mode and then starts saitama
 env\scripts\activate.bat && py -m SaitamaRobot
