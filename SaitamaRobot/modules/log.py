b70e9fe7cef243c161545bbaab06bee18c6d71c8
Accip <64995687+Accipiter7@users.noreply.github.com>
2020-08-24 20:10:48 +0530

    [ logs ] Restricted logs cmd

--- SaitamaRobot/modules/log.py
+++ SaitamaRobot/modules/log.py
@@ -9,20 +9,17 @@ from SaitamaRobot.modules.helper_funcs.chat_status import dev_plus
 @run_async
 @dev_plus
 def logs(update: Update, context: CallbackContext):
-
     message = update.effective_message
-
+    chat_id = message.chat_id
+    if chat_id != -1001230319386:
+         return
+    user = update.effective_user
     with open ('log.txt' ,'rb') as f:
 
         context.bot.send_document(
-
                 document=f,
-
                 filename=f.name,
-
-                reply_to_message_id=message.message_id,
-
-                chat_id=message.chat_id)
+                chat_id=user.id)
 
 LOG_HANDLER = CommandHandler('logs', logs)
 
