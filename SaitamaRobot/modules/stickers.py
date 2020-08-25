a440a00b8f9aba63eaf652c0b8e7f6f700ff0122
DragSama <49992845+DragSama@users.noreply.github.com>
2020-08-25 14:20:01 +0530

    Fix typo

--- SaitamaRobot/modules/stickers.py
+++ SaitamaRobot/modules/stickers.py
@@ -43,7 +43,7 @@ def cb_sticker(update: Update, context: CallbackContext):
     if len(split) == 1:
         msg.reply_text('Provide some name to search for pack.')
         return
-    text = requests.get(combot_stickers_url + split[1])
+    text = requests.get(combot_stickers_url + split[1]).text
     soup = bs(text, 'lxml')
     results  = soup.find_all("a",{'class':"sticker-pack__btn"})
     if not results:
