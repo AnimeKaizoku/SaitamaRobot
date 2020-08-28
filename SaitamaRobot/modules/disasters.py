d601946f7fc360b663f7f9c51ba6fbfae5dd4075
Sawada Tsunayoshi <animekaizokux@gmail.com>
2020-08-28 21:58:01 +0530

    Added extwaa !! for Disasters py

--- SaitamaRobot/modules/disasters.py
+++ SaitamaRobot/modules/disasters.py
@@ -529,7 +529,7 @@ def devlist(update: Update, context: CallbackContext):
 
 
 __help__ = f"""
-*⚠️ Notice:*
+*⚠️ Notice:⚠️*
 Commands listed here only work for users with special access are mainly used for troubleshooting, debugging purposes.
 Group admins/group owners do not need these commands. 
 
