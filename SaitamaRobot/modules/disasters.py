0c1a6558fb8eba2f245e3052b571db4bb7cc9250
Sawada Tsunayoshi <animekaizokux@gmail.com>
2020-08-28 22:31:39 +0530

    Adjustments to disasters help for OCD ridden users

--- SaitamaRobot/modules/disasters.py
+++ SaitamaRobot/modules/disasters.py
@@ -534,46 +534,49 @@ Commands listed here only work for users with special access are mainly used for
 Group admins/group owners do not need these commands. 
 
  ╔ *List all special users:*
- ╠ `/heroes`*:* Lists all Hero Association members.
  ╠ `/dragons`*:* Lists all Dragon disasters.
  ╠ `/demons`*:* Lists all Demon disasters.
  ╠ `/tigers`*:* Lists all Tigers disasters.
- ╚ `/wolves`*:* Lists all Wolf disasters.
+ ╠ `/wolves`*:* Lists all Wolf disasters.
+ ╚ `/heroes`*:* Lists all Hero Association members.
 
  ╔ *Ping:*
  ╠ `/ping`*:* gets ping time of bot to telegram server
  ╚ `/pingall`*:* gets all listed ping times
 
  ╔ *Broadcast: (Bot owner only)*
- ╠  Note: This supports basic markdown
- ╠ `/broadcastall messagehere`*:* Broadcasts the message everywhere.
- ╠ `/broadcastusers messagehere`*:* Broadcasts the message too all users of the bot.
- ╚ `/broadcastgroups messagehere`*:* Broadcasts the message too all groups of the bot.
+ ╠  *Note:* This supports basic markdown
+ ╠ `/broadcastall`*:* Broadcasts everywhere
+ ╠ `/broadcastusers`*:* Broadcasts too all users
+ ╚ `/broadcastgroups`*:* Broadcasts too all groups
 
  ╔ *Getchats:*
  ╚ `/getchats ID`*:* Gets a list of group names the user has been seen in. Bot owner only.
 
  ╔ *Blacklist:* 
- ╠ `/ignore`*:* Blacklists a user from using the bot entirely.
+ ╠ `/ignore`*:* Blacklists a user from 
+ ╠  using the bot entirely.
  ╚ `/notice`*:* Whitelists the user to allow bot usage. 
 
  ╔ *Speedtest:*
  ╚ `/speedtest`*:* Runs a speedtest and gives you 2 options to choose from, text or image output.
 
  ╔ *Global Bans:*
- ╠ `/gban user reason`*:* Globally bans a user anywhere the bot is admin in/will be admin in
+ ╠ `/gban user reason`*:* Globally bans a user.
  ╚ `/ungban user reason`*:* Unbans the user from the global bans list
 
  ╔ *Module loading:*
  ╠ `/listmodules`*:* Lists names of all modules
- ╠ `/load modulename`*:* Loads the said module to memory without restarting the bot
- ╚ `/unload modulename`*:* Loads the said module from memory without restarting the bot 
+ ╠ `/load modulename`*:* Loads the said module to 
+ ╠   memory without restarting.
+ ╠ `/unload modulename`*:* Loads the said module from
+ ╚   memory without restarting.memory without restarting the bot 
 
  ╔ *Remote commands:*
- ╠ `/runban usernamne/id groupusername/id`*:* Remotely bans said user in said group
- ╠ `/rpunch usernamne/id groupusername/id`*:* Remotely punches said user from said group
- ╠ `/rmute usernamne/id groupusername/id`*:* Remotely mutes said user in said group
- ╚ `/runmute usernamne/id groupusername/id`*:* Remotely unmutes said user in said group
+ ╠ `/runban user group`*:*
+ ╠ `/rpunch user group`*:*
+ ╠ `/rmute user group`*:*
+ ╚ `/runmute user group`*:*
 
  ╔ *Shell commands:* 
  ╠ `/eval`*:* Self explanatory
