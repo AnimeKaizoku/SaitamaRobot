0742bc1117b181c552c619c8df3419ff9e98224e
starry69 <starry369126@outlook.com>
2020-09-02 16:39:36 +0530

    Userinfo: add hpmanager function
    Signed-off-by: starry69 <starry369126@outlook.com>

--- SaitamaRobot/modules/userinfo.py
+++ SaitamaRobot/modules/userinfo.py
@@ -9,8 +9,10 @@ import SaitamaRobot.modules.sql.userinfo_sql as sql
 from SaitamaRobot import DEV_USERS, SUDO_USERS, dispatcher
 from SaitamaRobot.modules.disable import DisableAbleCommandHandler
 from SaitamaRobot.modules.helper_funcs.extraction import extract_user
-from SaitamaRobot.modules.sql.afk_sql import is_afk
+from SaitamaRobot.modules.sql.global_bans_sql import is_user_gbanned
+from SaitamaRobot.modules.sql.afk_sql import is_afk, check_afk_status
 from SaitamaRobot.modules.sql.users_sql import get_user_num_chats
+from SaitamaRobot.modules.sql.feds_sql import get_user_fbanlist
 from SaitamaRobot.modules.helper_funcs.chat_status import sudo_plus, user_admin
 from SaitamaRobot.modules.helper_funcs.extraction import extract_user
 
@@ -23,7 +25,6 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters
 from telegram.utils.helpers import mention_html
 
-
 def __user_info__(user_id):
     bio = html.escape(sql.get_user_bio(user_id) or "")
     me = html.escape(sql.get_user_me_info(user_id) or "")
@@ -35,6 +36,79 @@ def __user_info__(user_id):
         return f"<b>About user:</b>\n{me}"
     else:
         return ""
+
+
+def no_by_per(totalhp, percentage):
+    """
+    rtype: num of `percentage` from total
+    eg: 1000, 10 -> 10% of 1000 (100)
+    """
+    return totalhp * percentage / 100
+
+def get_percentage(totalhp,earnedhp):
+    """
+    rtype: percentage of `totalhp` num
+    eg: (1000, 100) will return 10%
+    """
+
+    matched_less = totalhp - earnedhp
+    per_of_totalhp = 100 - matched_less*100.0/totalhp
+    per_of_totalhp = str(int(per_of_totalhp)) + ' %'
+    return per_of_totalhp
+
+
+def hpmanager(user):
+    total_hp = (get_user_num_chats(user.id) + 10) * 10
+
+    if not is_user_gbanned(user.id):
+
+       # Assign new var `new_hp` since we need `total_hp` in
+       # end to calculate percentage.
+       new_hp = total_hp
+
+       # if no username decrease 25% of hp.
+       if not user.username:
+          new_hp -= no_by_per(new_hp, 25)
+       try:
+          dispatcher.bot.get_user_profile_photos(user.id).photos[0][-1]
+       except IndexError:
+          # no profile photo ==> -25% of hp
+          new_hp -= no_by_per(new_hp, 25)
+       # if no /setme exist ==> -20% of hp
+       if not sql.get_user_me_info(user.id):
+          new_hp -= no_by_per(new_hp, 20)
+       # if no bio exsit ==> -10% of hp
+       if not sql.get_user_bio(user.id):
+          new_hp -= no_by_per(new_hp, 10)
+
+       if is_afk(user.id):
+          afkst = check_afk_status(user.id)
+          # if user is afk and no reason then decrease 7%
+          # else if reason exist decrease 5%
+          if not afkst.reason:
+             new_hp -= no_by_per(new_hp, 7)
+          else:
+             new_hp -= no_by_per(new_hp, 5)
+
+       # fbanned users will have (2*number of fbans) less from max HP
+       # Example: if HP is 100 but user has 5 diff fbans
+       # Available HP is (2*5) = 10% less than Max HP
+       # So.. 10% of 100HP = 90HP
+
+       _, fbanlist = get_user_fbanlist(user.id)
+       new_hp -= no_by_per(new_hp, 2*len(fbanlist))
+
+    # Bad status effects:
+    # gbanned users will always have 5% HP from max HP
+    # Example: If HP is 100 but gbanned
+    # Available HP is 5% of 100 = 5HP
+
+    else:
+       new_hp = total_hp - no_by_per(total_hp, 5)
+
+    return {"totalhp": int(total_hp), "earnedhp": int(new_hp),
+            "percentage": get_percentage(total_hp,new_hp)}
+
 
 def make_bar(per):
      msg = ""
@@ -133,7 +207,7 @@ def info(update: Update, context: CallbackContext):
         text += f"\nUsername: @{html.escape(user.username)}"
 
     text += f"\nPermalink: {mention_html(user.id, 'link')}"
-    chat_count = get_user_num_chats(user.id)
+
     if chat.type != "private":
        _stext = "\nStatus: {}"
 
@@ -149,9 +223,10 @@ def info(update: Update, context: CallbackContext):
                   text += _stext.format("Present")
               elif status in {"administrator", "creator"}:
                   text += _stext.format("Admin")
-    hp = (chat_count+1)*50
-    percentage = (hp/10000)*100
-    text += f"\n<b>Health</b>: {hp}/10000\n<code>{make_bar(percentage)}</code> {percentage}%"
+
+    userhp = hpmanager(user)
+    text += f"\n\n<b>Health:</b> {userhp['totalhp']} / {userhp['earnedhp']}"
+
     try:
         spamwtc = sw.get_ban(int(user.id))
         if spamwtc:
