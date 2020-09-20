00f0ab97b3b9a7184f2a91842b32c0aca81bd0aa
TNO-TheMemer <71560042+TNO-TheMemer@users.noreply.github.com>
2020-09-20 19:57:48 +0530

    Update chatbot.py

--- SaitamaRobot/modules/chatbot.py
+++ SaitamaRobot/modules/chatbot.py
@@ -131,11 +131,11 @@ Chatbot utilizes the CoffeeHouse API and allows Saitama to talk and provides a m
 
 *Commands:* 
 *Admins only:*
- • `/addchat`*:* Enables Chatbot mode in the chat.
- • `/rmchat`*:* Disables Chatbot mode in the chat.
+ • /addchat*:* Enables Chatbot mode in the chat.
+ • /rmchat*:* Disables Chatbot mode in the chat.
  
 *Dragons or higher only:* 
- • `/listaichats`*:* Lists the chats the chatmode is enabled in.
+ • /listaichats*:* Lists the chats the chatmode is enabled in.
 
 Reports bugs at {SUPPORT_CHAT}
 *Powered by CoffeeHouse* (https://coffeehouse.intellivoid.net/) from @Intellivoid
