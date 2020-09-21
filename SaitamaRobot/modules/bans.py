f0db4acc17e56c5885449519343a0ba2e08dd2ce
Sawada Tsunayoshi <animekaizokux@gmail.com>
2020-09-21 08:33:16 +0530

    [bans] Broken attempt to improve ban replies

--- SaitamaRobot/modules/bans.py
+++ SaitamaRobot/modules/bans.py
@@ -89,11 +89,11 @@ def ban(update: Update, context: CallbackContext) -> str:
     try:
         chat.kick_member(user_id)
         # bot.send_sticker(chat.id, BAN_STICKER)  # banhammer marie sticker
-        bot.sendMessage(
-            chat.id,
-            "Banned user {}.".format(
-                mention_html(member.user.id, member.user.first_name)),
-            parse_mode=ParseMode.HTML)
+        reply = (f"<code>❕</code><b>Ban Event</b>\n"
+                 f"<code> </code><b>•  User:</b> {mention_html(member.user.id, member.user.first_name)}")
+        if reason:
+          reply += f"\n<code> </code><b>•  Reason:</b> {html.escape(reason)}"
+        bot.sendMessage(reply)
         return log
 
     except BadRequest as excp:
