1b2a14bfad1f60f60dc4a712f5d13d1e823cd624
DevilYashR <68998269+DevilYashR@users.noreply.github.com>
2020-09-21 18:39:13 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -53,7 +53,7 @@ And the following:
 SAITAMA_IMG = "https://telegra.ph/AsunaYuuki-09-21"
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
-Saitama is hosted on one of Kaizoku's Servers and doesn't require any donations as of now but \
+AsunaBot doesn't require any donations as of now but \
 You can donate to the original writer of the Base code, Paul
 There are two ways of supporting him; [PayPal](paypal.me/PaulSonOfLars), or [Monzo](monzo.me/paulnionvestergaardlarsen)."""
 
