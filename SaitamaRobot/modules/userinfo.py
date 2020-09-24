2ebcd696447a406af5d9fbbfeb36a1d72b3a36da
DragSama <49992845+DragSama@users.noreply.github.com>
2020-09-24 13:56:27 +0530

    Make error message bold

--- SaitamaRobot/modules/userinfo.py
+++ SaitamaRobot/modules/userinfo.py
@@ -151,9 +151,9 @@ async def group_info(event) -> None:
     chat = event.text.split(' ', 1)[1]
     try:
         entity = await event.client.get_entity(chat)
-            totallist = await event.client.get_participants(chat, filter=ChannelParticipantsAdmins)
+        totallist = await event.client.get_participants(chat, filter=ChannelParticipantsAdmins)
     except:
-        await event.reply('The channel specified is private and I lack permission to access it. Another reason may be that I am banned from it')
+        await event.reply('The channel specified is private and **I lack permission to access it**. Another reason may be that **I am banned from it**')
         return
     msg = f"**ID**: `{entity.id}`"
     msg += f"\n**Title**: `{entity.title}`"
