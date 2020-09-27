240483a952777e518c8620eabab942fa64104728
Sawada Tsunayoshi <animekaizokux@gmail.com>
2020-09-27 18:38:28 +0530

    [dbcleanup] Fix the attribute error

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -189,7 +189,7 @@ def start(update: Update, context: CallbackContext):
                 reply_markup=InlineKeyboardMarkup(
                     [
                     [InlineKeyboardButton(text="☑️ Add Saitama to your group", url="t.me/{}?startgroup=true".format(context.bot.username))],
-                    [InlineKeyboardButton(text="🚑 Support Group", url=f"https://t.me/{SUPPORT_CHAT}"), InlineKeyboardButton(text="🔔 Update Channel", url="https://t.me/OnePunchUpdates")],
+                    [InlineKeyboardButton(text="🚑 Support Group", url=f"https://t.me/{SUPPORT_CHAT}"), InlineKeyboardButton(text="🔔 Updates Channel", url="https://t.me/OnePunchUpdates")],
                     [InlineKeyboardButton(text="🧾 Getting started guide", url="https://t.me/OnePunchUpdates/29")],
                     [InlineKeyboardButton(text="🗄 Source code", url="https://github.com/AnimeKaizoku/SaitamaRobot")]
                     ]
--- SaitamaRobot/modules/dbcleanup.py
+++ SaitamaRobot/modules/dbcleanup.py
@@ -92,10 +92,10 @@ def dbcleanup(update: Update, context: CallbackContext):
     msg = update.effective_message
 
     msg.reply_text("Getting invalid chat count ...")
-    invalid_chat_count = get_invalid_chats(context, update)
+    invalid_chat_count = get_invalid_chats(update, context)
 
     msg.reply_text("Getting invalid gbanned count ...")
-    invalid_gban_count = get_invalid_gban(context, update)
+    invalid_gban_count = get_invalid_gban(update, context)
 
     reply = f"Total invalid chats - {invalid_chat_count}\n"
     reply += f"Total invalid gbanned users - {invalid_gban_count}"
