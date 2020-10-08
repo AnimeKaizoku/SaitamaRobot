6895003d6897960c5e83086c5f762d2a7c1b176c
Roronoa Zoro <ramakrishnan4510@gmail.com>
2020-10-08 15:04:01 +0530

    Updated __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -188,7 +188,7 @@ def start(update: Update, context: CallbackContext):
                 reply_markup=InlineKeyboardMarkup(
                     [[
                         InlineKeyboardButton(
-                            text="☑️ Add Saitama to your group",
+                            text="☑️ Add Mai Sakurajima to your group",
                             url="t.me/{}?startgroup=true".format(
                                 context.bot.username))
                     ],
