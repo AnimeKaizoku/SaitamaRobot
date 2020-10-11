edc220cdabb079c6d4fad623c937eaf874591c81
Sawada Tsunayoshi <animekaizokux@gmail.com>
2020-10-11 17:08:57 +0530

    [critical] Fix bad permissions for disasters

--- SaitamaRobot/modules/helper_funcs/telethn/chatstatus.py
+++ SaitamaRobot/modules/helper_funcs/telethn/chatstatus.py
@@ -1,4 +1,5 @@
 from SaitamaRobot.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
+from SaitamaRobot import SUDO_USERS
 from telethon.tl.types import ChannelParticipantsAdmins
 
 
@@ -22,7 +23,7 @@ async def user_is_admin(user_id: int, message):
 
     async for user in telethn.iter_participants(
             message.chat_id, filter=ChannelParticipantsAdmins):
-        if user_id == user.id or user_id in IMMUNE_USERS:
+        if user_id == user.id or user_id in SUDO_USERS:
             status = True
             break
     return status
@@ -32,7 +33,7 @@ async def is_user_admin(user_id: int, chat_id):
     status = False
     async for user in telethn.iter_participants(
             chat_id, filter=ChannelParticipantsAdmins):
-        if user_id == user.id or user_id in IMMUNE_USERS:
+        if user_id == user.id or user_id in SUDO_USERS:
             status = True
             break
     return status
