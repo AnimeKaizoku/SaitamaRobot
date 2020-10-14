a86462dcc522dfa837f66f0174f696497af4f5b1
Accip <64995687+Accipiter7@users.noreply.github.com>
2020-10-14 14:51:43 +0530

    improved purging logic

--- SaitamaRobot/modules/purge.py
+++ SaitamaRobot/modules/purge.py
@@ -1,73 +1,42 @@
-from SaitamaRobot.modules.helper_funcs.telethn.chatstatus import (
-    can_delete_messages, user_is_admin)
-from SaitamaRobot import telethn
-import time
-from telethon import events
+from pyrogram import filters
+from SaitamaRobot import app
+import asyncio
+from pyrogram import raw
 
+async def user_can_purge(_, c, m):
+    mem = await c.get_chat_member(chat_id=m.chat.id, user_id=m.from_user.id)
+    if mem.can_delete_messages or mem.status in ("creator"):
+        return True
+    await m.reply("Only Admins are allowed to use this command")
+   
+can_purge = filters.create(user_can_purge)
 
-@telethn.on(events.NewMessage(pattern="^[!/]purge$"))
-async def purge_messages(event):
-    start = time.perf_counter()
-    if event.from_id is None:
+@app.on_message(filters.command("purge")& filters.reply & can_purge, group=0)
+async def purge(c, m):
+    messages = list(range(m.message_id + 1, m.reply_to_message.message_id, -1))
+    while len(messages) != 0:
+        await c.send(
+            raw.functions.channels.DeleteMessages(
+                channel=await c.resolve_peer(m.chat.id), id=messages))
+        del messages[0:100]
+    await c.send_message(
+        text="**Fast Purge Completed!!!**",
+        chat_id=m.chat.id)
+
+@app.on_message(filters.command('purge')& ~filters.reply & can_purge)
+async def int_purge(c, m):
+    try:
+        count = int(m.command[1])
+    except IndexError:
+        await m.reply("__specifiy number of messages to delete__")
         return
+    messages = list(range(m.message_id - 1, m.message_id - 1 - count, -1))
+    start, end = messages[-1], messages[0]
+    while len(messages) != 0:
+        await c.send(
+            raw.functions.channels.DeleteMessages(
+                channel=await c.resolve_peer(m.chat.id), id=messages))
+        del messages[0:100]
+    await c.send_message(
+        text=f"Purged {count} messages from {start} to {end}",chat_id=m.chat.id)
 
-    if not await user_is_admin(user_id=event.from_id, message=event):
-        await event.reply("Only Admins are allowed to use this command")
-        return
-
-    if not await can_delete_messages(message=event):
-        await event.reply("Can't seem to purge the message")
-        return
-
-    reply_msg = await event.get_reply_message()
-    if not reply_msg:
-        await event.reply(
-            "Reply to a message to select where to start purging from.")
-        return
-    messages = []
-    message_id = reply_msg.id
-    delete_to = event.message.id
-
-    messages.append(event.reply_to_msg_id)
-    for msg_id in range(message_id, delete_to + 1):
-        messages.append(msg_id)
-        if len(messages) == 100:
-            await event.client.delete_messages(event.chat_id, messages)
-            messages = []
-
-    await event.client.delete_messages(event.chat_id, messages)
-    time_ = time.perf_counter() - start
-    text = f"Purged Successfully in {time_:0.2f} Second(s)"
-    await event.respond(text, parse_mode='markdown')
-
-
-@telethn.on(events.NewMessage(pattern="^[!/]del$"))
-async def delete_messages(event):
-    if event.from_id is None:
-        return
-
-    if not await user_is_admin(user_id=event.from_id, message=event):
-        await event.reply("Only Admins are allowed to use this command")
-        return
-
-    if not await can_delete_messages(message=event):
-        await event.reply("Can't seem to delete this?")
-        return
-
-    message = await event.get_reply_message()
-    if not message:
-        await event.reply("Whadya want to delete?")
-        return
-    chat = await event.get_input_chat()
-    del_message = [message, event.message]
-    await event.client.delete_messages(chat, del_message)
-
-
-__help__ = """
-*Admin only:*
- - /del: deletes the message you replied to
- - /purge: deletes all messages between this and the replied to message.
- - /purge <integer X>: deletes the replied message, and X messages following it if replied to a message.
-"""
-
-__mod_name__ = "Purges"
