a4f22bcd293281388eea6f39e3ffb2d6337d9109
Accip <64995687+Accipiter7@users.noreply.github.com>
2020-10-14 15:09:14 +0530

    rewritten currency converter
    simplified a lot
    fully async

--- SaitamaRobot/modules/currency_converter.py
+++ SaitamaRobot/modules/currency_converter.py
@@ -1,54 +1,24 @@
-import requests
-from SaitamaRobot import CASH_API_KEY, dispatcher
-from telegram import Update, ParseMode
-from telegram.ext import CallbackContext, CommandHandler, run_async
+import aiohttp
+from pyrogram import filters
+from bs4 import BeautifulSoup as bs
+from SaitamaRobot import app
 
 
-@run_async
-def convert(update: Update, context: CallbackContext):
-    args = update.effective_message.text.split(" ")
-
-    if len(args) == 4:
-        try:
-            orig_cur_amount = float(args[1])
-
-        except ValueError:
-            update.effective_message.reply_text("Invalid Amount Of Currency")
-            return
-
-        orig_cur = args[2].upper()
-
-        new_cur = args[3].upper()
-
-        request_url = (f"https://www.alphavantage.co/query"
-                       f"?function=CURRENCY_EXCHANGE_RATE"
-                       f"&from_currency={orig_cur}"
-                       f"&to_currency={new_cur}"
-                       f"&apikey={CASH_API_KEY}")
-        response = requests.get(request_url).json()
-        try:
-            current_rate = float(
-                response['Realtime Currency Exchange Rate']['5. Exchange Rate'])
-        except KeyError:
-            update.effective_message.reply_text("Currency Not Supported.")
-            return
-        new_cur_amount = round(orig_cur_amount * current_rate, 5)
-        update.effective_message.reply_text(
-            f"{orig_cur_amount} {orig_cur} = {new_cur_amount} {new_cur}")
-
-    elif len(args) == 1:
-        update.effective_message.reply_text(
-            __help__, parse_mode=ParseMode.MARKDOWN)
-
-    else:
-        update.effective_message.reply_text(
-            f"*Invalid Args!!:* Required 3 But Passed {len(args) -1}",
-            parse_mode=ParseMode.MARKDOWN)
-
-
-CONVERTER_HANDLER = CommandHandler('cash', convert)
-
-dispatcher.add_handler(CONVERTER_HANDLER)
-
-__command_list__ = ["cash"]
-__handlers__ = [CONVERTER_HANDLER]
+@app.on_message(filters.command('cash'))
+async def curr_converter(c, m):
+    try:
+        amount = m.command[1]
+        from_, to = m.command[2], m.command[3]
+    except IndexError:
+        await m.reply("Invalid Format")
+        return
+    query = f"https://www.x-rates.com/calculator/?from={from_}&to={to}&amount={amount}"
+    async with aiohttp.ClientSession() as sess:
+        async with sess.get(query) as resp:
+            data = await resp.text()
+    soup = bs(data, 'lxml')
+    result = soup.find('span', class_="ccOutputRslt").get_text()
+    if '-' in result:
+        await m.reply("Invalid Currency Format")
+        return
+    await m.reply(f"__{amount} {from_.upper()}__ = **{result}**")
