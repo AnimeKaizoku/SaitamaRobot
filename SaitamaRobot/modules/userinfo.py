59b5d78ce32935c04a826ef456c719a683106671
nidhirobot <73631177+nidhirobot@users.noreply.github.com>
2020-10-29 20:31:23 +0530

    Update userinfo.py

--- SaitamaRobot/modules/userinfo.py
+++ SaitamaRobot/modules/userinfo.py
@@ -291,7 +291,7 @@ def info(update: Update, context: CallbackContext):
         disaster_level_present = True
 
     if disaster_level_present:
-        text += ' [<a href="https://t.me/OnePunchUpdates/155">?</a>]'.format(
+        text += ' [<a href="https://t.me/nidhiupdates/4">?</a>]'.format(
             bot.username)
 
     try:
