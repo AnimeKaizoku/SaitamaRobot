225f1ddf2330aeaf342208b1be75c9485f849043
nidhirobot <73631177+nidhirobot@users.noreply.github.com>
2020-10-30 01:02:11 +0530

    Update backups.py

--- SaitamaRobot/modules/backups.py
+++ SaitamaRobot/modules/backups.py
@@ -335,7 +335,7 @@ def export_data(update, context):
     context.bot.sendDocument(
         current_chat_id,
         document=open("SaitamaRobot{}.backup".format(chat_id), "rb"),
-        caption="*Successfully Exported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n\nNote: This `SaitamaRobot-Backup` was specially made for notes."
+        caption="*Successfully Exported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n\nNote: *Your data backup successfully😇, join @nidhisupportchat.*\n"
         .format(chat.title, chat_id, tgl),
         timeout=360,
         reply_to_message_id=msg.message_id,
