e9cf6fa1ec41f016b8310f4c6d723433d6faba78
nidhirobot <73631177+nidhirobot@users.noreply.github.com>
2020-10-29 13:00:13 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -53,19 +53,18 @@ def get_readable_time(seconds: int) -> str:
 
 PM_START_TEXT = """
 Hi {}, my name is {}! 
-I am an Anime themed group management bot.
-Build by weebs for weebs, I specialize in managing anime and similar themed groups.
+I am group management bot.
+currently running in testing mode.
 You can find my list of available commands with /help.
 """
 
 HELP_STRINGS = """
 Hey there! My name is *{}*.
-I'm a Hero For Fun and help admins manage their groups with One Punch! Have a look at the following for an idea of some of \
+I'm perfect one to manage your groups!! Have a look at the following for an idea of some of \
 the things I can help you with.
 *Main* commands available:
  • /help: PM's you this message.
  • /help <module name>: PM's you info about that module.
- • /donate: information on how to donate!
  • /settings:
    • in PM: will send you your settings for all supported modules.
    • in a group: will redirect you to pm, with all that chat's settings.
