c193efb3b0629eb49f3340b77ae372755703d9c2
nidhirobot <73631177+nidhirobot@users.noreply.github.com>
2020-10-29 12:37:01 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -75,12 +75,14 @@ the things I can help you with.
 {}
 And the following:
 """.format(
-    dispatcher.bot.first_name, """
+    dispatcher.bot.first_name,"""
     if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
 SAITAMA_IMG = "https://telegra.ph/file/04c51a35243f1b2d4ab14.jpg"
 
-DONATE_STRING = """Currently i'm running on free server so no need of donations @nidhisupportchat"""
+DONATE_STRING = """
+  Currently i'm running on free server so no need of donations @nidhisupportchat
+  """
 
 IMPORTED = {}
 MIGRATEABLE = []
