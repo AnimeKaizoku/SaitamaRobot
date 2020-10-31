65de5109202ef3f380a2b2983a595b07e634e2f8
nidhirobot <73631177+nidhirobot@users.noreply.github.com>
2020-10-31 18:43:05 +0530

    Update userinfo.py

--- SaitamaRobot/modules/userinfo.py
+++ SaitamaRobot/modules/userinfo.py
@@ -517,7 +517,7 @@ Examples:
  • `/info`*:* get information about a user. 
  
 *What is that health thingy?*
- Come and see [HP System explained](https://t.me/OnePunchUpdates/192)
+ @nidhisupportchat)
 """
 
 SET_BIO_HANDLER = DisableAbleCommandHandler("setbio", set_about_bio)
