764bdb74d85c1d948f188841166677343ec0578b
Sasuke Uchiha <73424435+FtSasuke@users.noreply.github.com>
2020-11-05 09:10:20 +0530

    Updated to Senku one

--- SaitamaRobot/modules/chatbot.py
+++ SaitamaRobot/modules/chatbot.py
@@ -131,7 +131,7 @@ def list_chatbot_chats(update: Update, context: CallbackContext):
 
 
 __help__ = f"""
-Chatbot utilizes the CoffeeHouse API and allows Saitama to talk and provides a more interactive group chat experience.
+Chatbot utilizes the CoffeeHouse API and allows Senku to talk and provides a more interactive group chat experience.
 
 *Commands:* 
 *Admins only:*
