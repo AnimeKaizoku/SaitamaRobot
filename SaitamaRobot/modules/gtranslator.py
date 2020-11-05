bf416a662efc8d5f6440c27abb791f42a26eeef4
Sasuke Uchiha <73424435+FtSasuke@users.noreply.github.com>
2020-11-05 08:53:08 +0530

    SI

--- SaitamaRobot/modules/gtranslator.py
+++ SaitamaRobot/modules/gtranslator.py
@@ -116,7 +116,7 @@ def totranslate(update: Update, context: CallbackContext):
             "Reply to messages or write messages from other languages ​​for translating into the intended language\n\n"
             "Example: `/tr en-ml` to translate from English to Malayalam\n"
             "Or use: `/tr ml` for automatic detection and translating it into Malayalam.\n"
-            "See [List of Language Codes](t.me/OnePunchSupport/12823) for a list of language codes.",
+            "See ✨[List of Language Codes](t.me/SenkuUpdates/20) for a list of language codes.",
             parse_mode="markdown",
             disable_web_page_preview=True)
     except ValueError:
