402d58feaf93a05a9de5abd3c3a785ae0ccf35d3
Sasuke Uchiha <73424435+FtSasaki@users.noreply.github.com>
2020-11-06 10:05:20 +0530

    Create drama.py

---
+++ SaitamaRobot/modules/drama.py
@@ -0,0 +1,492 @@
+import datetime
+import html
+import textwrap
+
+import bs4
+import jikanpy
+import requests
+from SaitamaRobot import DEV_USERS, OWNER_ID, DRAGONS, dispatcher
+from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from telegram import (InlineKeyboardButton, InlineKeyboardMarkup, ParseMode,
+                      Update)
+from telegram.ext import CallbackContext, CallbackQueryHandler, run_async
+
+info_btn = "More Information"
+kaizoku_btn = "Kaizoku ☠️"
+kayo_btn = "Kayo 🏴‍☠️"
+prequel_btn = "⬅️ Prequel"
+sequel_btn = "Sequel ➡️"
+close_btn = "Close ❌"
+
+
+def shorten(description, info='mydramalist.com'):
+    msg = ""
+    if len(description) > 700:
+        description = description[0:500] + '....'
+        msg += f"\n*Description*: _{description}_[Read More]({info})"
+    else:
+        msg += f"\n*Description*:_{description}_"
+    return msg
+
+
+#time formatter from uniborg
+def t(milliseconds: int) -> str:
+    """Inputs time in milliseconds, to get beautified time,
+    as string"""
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = ((str(days) + " Days, ") if days else "") + \
+        ((str(hours) + " Hours, ") if hours else "") + \
+        ((str(minutes) + " Minutes, ") if minutes else "") + \
+        ((str(seconds) + " Seconds, ") if seconds else "") + \
+        ((str(milliseconds) + " ms, ") if milliseconds else "")
+    return tmp[:-2]
+
+
+airing_query = '''
+    query ($id: Int,$search: String) { 
+      Media (id: $id, type: search: $search) { 
+        id
+        episodes
+        title {
+          romaji
+          english
+          native
+        }
+        nextAiringEpisode {
+           airingAt
+           timeUntilAiring
+           episode
+        } 
+      }
+    }
+    '''
+
+fav_query = """
+query ($id: Int) { 
+      Media (id: $id, type: ANIME) { 
+        id
+        title {
+          romaji
+          english
+          native
+        }
+     }
+}
+"""
+
+anime_query = '''
+   query ($id: Int,$search: String) { 
+      Media (id: $id, type: search: $search) { 
+        id
+        title {
+          romaji
+          english
+          native
+        }
+        description (asHtml: false)
+        startDate{
+            year
+          }
+          episodes
+          season
+          type
+          format
+          status
+          duration
+          siteUrl
+          studios{
+              nodes{
+                   name
+              }
+          }
+          trailer{
+               id
+               site 
+               thumbnail
+          }
+          averageScore
+          genres
+          bannerImage
+      }
+    }
+'''
+character_query = """
+    query ($query: String) {
+        Character (search: $query) {
+               id
+               name {
+                     first
+                     last
+                     full
+               }
+               siteUrl
+               image {
+                        large
+               }
+               description
+        }
+    }
+"""
+
+url = 'https://graphql.mydramalist.com'
+
+
+@run_async
+def diring(update: Update, context: CallbackContext):
+    message = update.effective_message
+    search_str = message.text.split(' ', 1)
+    if len(search_str) == 1:
+        update.effective_message.reply_text(
+            'Tell Drama Name :) ( /airing <drama name>)')
+        return
+    variables = {'search': search_str[1]}
+    response = requests.post(
+        url, json={
+            'query': airing_query,
+            'variables': variables
+        }).json()['data']['Media']
+    msg = f"*Name*: *{response['title']['romaji']}*(`{response['title']['native']}`)\n*ID*: `{response['id']}`"
+    if response['nextAiringEpisode']:
+        time = response['nextAiringEpisode']['timeUntilAiring'] * 1000
+        time = t(time)
+        msg += f"\n*Episode*: `{response['nextAiringEpisode']['episode']}`\n*Airing In*: `{time}`"
+    else:
+        msg += f"\n*Episode*:{response['episodes']}\n*Status*: `N/A`"
+    update.effective_message.reply_text(msg, parse_mode=ParseMode.MARKDOWN)
+
+
+@run_async
+def drama(update: Update, context: CallbackContext):
+    message = update.effective_message
+    search = message.text.split(' ', 1)
+    if len(search) == 1:
+        update.effective_message.reply_text('Format : /anime < drama name >')
+        return
+    else:
+        search = search[1]
+    variables = {'search': search}
+    json = requests.post(
+        url, json={
+            'query': anime_query,
+            'variables': variables
+        }).json()
+    if 'errors' in json.keys():
+        update.effective_message.reply_text('Drama not found')
+        return
+    if json:
+        json = json['data']['Media']
+        msg = f"*{json['title']['romaji']}*(`{json['title']['native']}`)\n*Type*: {json['format']}\n*Status*: {json['status']}\n*Episodes*: {json.get('episodes', 'N/A')}\n*Duration*: {json.get('duration', 'N/A')} Per Ep.\n*Score*: {json['averageScore']}\n*Genres*: `"
+        for x in json['genres']:
+            msg += f"{x}, "
+        msg = msg[:-2] + '`\n'
+        msg += "*Original Network*: `"
+        for x in json['studios']['nodes']:
+            msg += f"{x['name']}, "
+        msg = msg[:-2] + '`\n'
+        info = json.get('siteUrl')
+        trailer = json.get('trailer', None)
+        anime_id = json['id']
+        if trailer:
+            trailer_id = trailer.get('id', None)
+            site = trailer.get('site', None)
+            if site == "youtube":
+                trailer = 'https://youtu.be/' + trailer_id
+        description = json.get('description', 'N/A').replace('<i>', '').replace(
+            '</i>', '').replace('<br>', '')
+        msg += shorten(description, info)
+        image = json.get('bannerImage', None)
+        if trailer:
+            buttons = [[  
+                InlineKeyboardButton("More Info", url=info),
+                InlineKeyboardButton("Trailer 🎬", url=trailer)
+            ]]
+        else:
+            buttons = [[InlineKeyboardButton("More Info", url=info)]]
+        if image:
+            try:
+                update.effective_message.reply_photo(
+                    photo=image,
+                    caption=msg,
+                    parse_mode=ParseMode.MARKDOWN,
+                    reply_markup=InlineKeyboardMarkup(buttons))
+            except:
+                msg += f" [〽️]({image})"
+                update.effective_message.reply_text(
+                    msg,
+                    parse_mode=ParseMode.MARKDOWN,
+                    reply_markup=InlineKeyboardMarkup(buttons))
+        else:
+            update.effective_message.reply_text(
+                msg,
+                parse_mode=ParseMode.MARKDOWN,
+                reply_markup=InlineKeyboardMarkup(buttons))
+
+
+@run_async
+def artist(update: Update, context: CallbackContext):
+    message = update.effective_message
+    search = message.text.split(' ', 1)
+    if len(search) == 1:
+        update.effective_message.reply_text(
+            'Format : /artist < artist name >')
+        return
+    search = search[1]
+    variables = {'query': search}
+    json = requests.post(
+        url, json={
+            'query': character_query,
+            'variables': variables
+        }).json()
+    if 'errors' in json.keys():
+        update.effective_message.reply_text('Character not found')
+        return
+    if json:
+        json = json['data']['Character']
+        msg = f"*{json.get('name').get('full')}*(`{json.get('name').get('native')}`)\n"
+        description = f"{json['description']}"
+        site_url = json.get('siteUrl')
+        msg += shorten(description, site_url)
+        image = json.get('image', None)
+        if image:
+            image = image.get('large')
+            update.effective_message.reply_photo(
+                photo=image,
+                caption=msg.replace('<b>', '</b>'),
+                parse_mode=ParseMode.MARKDOWN)
+        else:
+            update.effective_message.reply_text(
+                msg.replace('<b>', '</b>'), parse_mode=ParseMode.MARKDOWN)
+
+
+@run_async
+def fren(update: Update, context: CallbackContext):
+    message = update.effective_message
+    args = message.text.strip().split(" ", 1)
+
+    try:
+        search_query = args[1]
+    except:
+        if message.reply_to_message:
+            search_query = message.reply_to_message.text
+        else:
+            update.effective_message.reply_text("Format : /user <username>")
+            return
+
+    jikan = jikanpy.jikan.Jikan()
+
+    try:
+        user = jikan.user(search_query)
+    except jikanpy.APIException:
+        update.effective_message.reply_text("Username not found.")
+        return
+
+    progress_message = update.effective_message.reply_text("Searching.... ")
+
+    date_format = "%Y-%m-%d"
+    if user['image_url'] is None:
+        img = "https://cdn.myanimelist.net/images/questionmark_50.gif"
+    else:
+        img = user['image_url']
+
+    try:
+        user_birthday = datetime.datetime.fromisoformat(user['birthday'])
+        user_birthday_formatted = user_birthday.strftime(date_format)
+    except:
+        user_birthday_formatted = "Unknown"
+
+    user_joined_date = datetime.datetime.fromisoformat(user['joined'])
+    user_joined_date_formatted = user_joined_date.strftime(date_format)
+
+    for entity in user:
+        if user[entity] is None:
+            user[entity] = "Unknown"
+
+    about = user['about'].split(" ", 60)
+
+    try:
+        about.pop(60)
+    except IndexError:
+        pass
+
+    about_string = ' '.join(about)
+    about_string = about_string.replace("<br>",
+                                        "").strip().replace("\r\n", "\n")
+
+    caption = ""
+
+    caption += textwrap.dedent(f"""
+    *Username*: [{user['username']}]({user['url']})
+
+    *Gender*: `{user['gender']}`
+    *Birthday*: `{user_birthday_formatted}`
+    *Joined*: `{user_joined_date_formatted}`
+    *Days wasted watching drama*: `{user['anime_stats']['days_watched']}`
+
+    """)
+
+    caption += f"*About*: {about_string}"
+
+    buttons = [[InlineKeyboardButton(info_btn, url=user['url'])],
+               [
+                   InlineKeyboardButton(
+                       close_btn,
+                       callback_data=f"anime_close, {message.from_user.id}")
+               ]]
+
+    update.effective_message.reply_photo(
+        photo=img,
+        caption=caption,
+        parse_mode=ParseMode.MARKDOWN,
+        reply_markup=InlineKeyboardMarkup(buttons),
+        disable_web_page_preview=False)
+    progress_message.delete()
+
+
+@run_async
+def button(update: Update, context: CallbackContext):
+    bot = context.bot
+    query = update.callback_query
+    message = query.message
+    data = query.data.split(", ")
+    query_type = data[0]
+    original_user_id = int(data[1])
+
+    user_and_admin_list = [original_user_id, OWNER_ID] + DRAGONS + DEV_USERS
+
+    bot.answer_callback_query(query.id)
+    if query_type == "anime_close":
+        if query.from_user.id in user_and_admin_list:
+            message.delete()
+        else:
+            query.answer("You are not allowed to use this.")
+    elif query_type in ('anime_anime', 'anime_manga'):
+        mal_id = data[2]
+        if query.from_user.id == original_user_id:
+            message.delete()
+            progress_message = bot.sendMessage(message.chat.id,
+                                               "Searching.... ")
+            caption, buttons, image = get_anime_manga(mal_id, query_type,
+                                                      original_user_id)
+            bot.sendPhoto(
+                message.chat.id,
+                photo=image,
+                caption=caption,
+                parse_mode=ParseMode.HTML,
+                reply_markup=InlineKeyboardMarkup(buttons),
+                disable_web_page_preview=False)
+            progress_message.delete()
+        else:
+            query.answer("You are not allowed to use this.")
+
+
+def site_search(update: Update, context: CallbackContext, site: str):
+    message = update.effective_message
+    args = message.text.strip().split(" ", 1)
+    more_results = True
+
+    try:
+        search_query = args[1]
+    except IndexError:
+        message.reply_text("Give something to search")
+        return
+
+    if site == "mla":
+        search_url = f"https://mydramalist.com/?s={search_query}"
+        html_text = requests.get(search_url).text
+        soup = bs4.BeautifulSoup(html_text, "html.parser")
+        search_result = soup.find_all("h2", {'class': "post-title"})
+
+        if search_result:
+            result = f"<b>Search results for</b> <code>{html.escape(search_query)}</code> <b>on</b> <code>MyDramaList</code>: \n"
+            for entry in search_result:
+                post_link = "https://mydramalist.com/" + entry.a['href']
+                post_name = html.escape(entry.text)
+                result += f"• <a href='{post_link}'>{post_name}</a>\n"
+        else:
+            more_results = False
+            result = f"<b>No result found for</b> <code>{html.escape(search_query)}</code> <b>on</b> <code>MyDramaList</code>"
+
+    elif site == "mydl":
+        search_url = f"https://mydramalist.com/?s={search_query}"
+        html_text = requests.get(search_url).text
+        soup = bs4.BeautifulSoup(html_text, "html.parser")
+        search_result = soup.find_all("h2", {'class': "title"})
+
+        result = f"<b>Search results for</b> <code>{html.escape(search_query)}</code> <b>on</b> <code>MyDramaList</code>: \n"
+        for entry in search_result:
+
+            if entry.text.strip() == "Nothing Found":
+                result = f"<b>No result found for</b> <code>{html.escape(search_query)}</code> <b>on</b> <code>MyDramaList</code>"
+                more_results = False
+                break
+
+            post_link = entry.a['href']
+            post_name = html.escape(entry.text.strip())
+            result += f"• <a href='{post_link}'>{post_name}</a>\n"
+
+    buttons = [[InlineKeyboardButton("See all results", url=search_url)]]
+
+    if more_results:
+        message.reply_text(
+            result,
+            parse_mode=ParseMode.HTML,
+            reply_markup=InlineKeyboardMarkup(buttons),
+            disable_web_page_preview=True)
+    else:
+        message.reply_text(
+            result, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
+
+
+@run_async
+def mla(update: Update, context: CallbackContext):
+    site_search(update, context, "mdl")
+
+
+@run_async
+def mydl(update: Update, context: CallbackContext):
+    site_search(update, context, "mydl")
+
+
+__help__ = """
+Get information about anime, manga or characters from [MDL](mydramalist.com).
+
+*Available commands:*
+
+ • `/drama <drama>`*:* returns information about the anime.
+ • `/artist <artist>`*:* returns information about the character.
+ • `/fren <user>`*:* returns information about a MyAnimeList user.
+ • `/mdl <drama>`*:* search an drama on MDL
+ • `/mydl <drama>`*:* search an drama on MDL
+ • `/diring <drama>`*:* returns drama airing info.
+
+ """
+
+DRAMA_HANDLER = DisableAbleCommandHandler("drama", drama)
+ARTIST_HANDLER = DisableAbleCommandHandler("artist", artist)
+FREN_HANDLER = DisableAbleCommandHandler("fren", fren)
+DIRING_HANDLER = DisableAbleCommandHandler("diring", diring)
+MDL_SEARCH_HANDLER = DisableAbleCommandHandler("mdl", mdl)
+MYDL_SEARCH_HANDLER = DisableAbleCommandHandler("mydl", mydl)
+BUTTON_HANDLER = CallbackQueryHandler(button, pattern='anime_.*')
+
+dispatcher.add_handler(BUTTON_HANDLER)
+dispatcher.add_handler(DRAMA_HANDLER)
+dispatcher.add_handler(DIRING_HANDLER)
+dispatcher.add_handler(ARTIST_HANDLER)
+dispatcher.add_handler(FREN_HANDLER)
+dispatcher.add_handler(MDL_HANDLER)
+dispatcher.add_handler(MYDL_HANDLER)
+
+__mod_name__ = "Drama"
+__command_list__ = [
+    "drama", "artist", "user", "mdl", "mydl", "fren", "diring",
+   ]
+   
+__handlers__ = [
+    DRAMA_HANDLER, ARTIST_HANDLER, FREN_HANDLER,
+    MDL_SEARCH_HANDLER, MyDL_SEARCH_HANDLER,
+    BUTTON_HANDLER, DAIRING_HANDLER
+]
