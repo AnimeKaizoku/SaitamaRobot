30168b97959f75fd2cfbb3cc5ef0621088fd19d7
Sawada Tsunayoshi <animekaizokux@gmail.com>
2020-11-09 22:49:32 +0530

    [main] edit uptime response a bit

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -225,7 +225,7 @@ def start(update: Update, context: CallbackContext):
                      ]]))
     else:
         update.effective_message.reply_text(
-            "I'm online!\n<b>Up since:</b> <code>{}</code>".format(uptime),
+            "I'm awake already!\n<b>Haven't slept since:</b> <code>{}</code>".format(uptime),
             parse_mode=ParseMode.HTML)
 
 
