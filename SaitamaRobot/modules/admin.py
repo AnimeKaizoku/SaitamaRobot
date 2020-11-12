a4039b639cb161d73977d2bb8f8fe10b2e0a24d7
el0xren <66721313+el0xren@users.noreply.github.com>
2020-11-12 15:27:17 +0300

    Minor changes

--- SaitamaRobot/modules/admin.py
+++ SaitamaRobot/modules/admin.py
@@ -5,18 +5,18 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 
-from SaitamaRobot import DRAGONS, dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.helper_funcs.chat_status import (bot_admin, can_pin,
+from sexlorge import DRAGONS, dispatcher
+from sexlorge.modules.disable import DisableAbleCommandHandler
+from sexlorge.modules.helper_funcs.chat_status import (bot_admin, can_pin,
                                                            can_promote,
                                                            connection_status,
                                                            user_admin,
                                                            ADMIN_CACHE)
 
-from SaitamaRobot.modules.helper_funcs.extraction import (extract_user,
+from sexlorge.modules.helper_funcs.extraction import (extract_user,
                                                           extract_user_and_text)
-from SaitamaRobot.modules.log_channel import loggable
-from SaitamaRobot.modules.helper_funcs.alternate import send_message
+from sexlorge.modules.log_channel import loggable
+from sexlorge.modules.helper_funcs.alternate import send_message
 
 
 @run_async
