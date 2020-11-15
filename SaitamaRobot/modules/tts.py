da5402c5482eb996d9e025c8793336c6125ea5e2
Uday0011 <73984830+Uday0011@users.noreply.github.com>
2020-11-15 18:42:51 +0530

    Create tts.py

---
+++ SaitamaRobot/modules/tts.py
@@ -0,0 +1,45 @@
+from telegram import ChatAction
+from gtts import gTTS
+import html
+import urllib.request
+import re
+import json
+from datetime import datetime
+from typing import Optional, List
+import time
+import requests
+from telegram import Message, Chat, Update, Bot, MessageEntity
+from telegram import ParseMode
+from telegram.ext import CommandHandler, run_async, Filters
+from telegram.utils.helpers import escape_markdown, mention_html
+from SaitamaRobot import dispatcher
+from SaitamaRobot.__main__ import STATS
+from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from SaitamaRobot.modules.helper_funcs.extraction import extract_user
+
+def tts(update, context):
+    args = context.args
+    current_time = datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M:%S")
+    filename = datetime.now().strftime("%d%m%y-%H%M%S%f")
+    reply = " ".join(args)
+    update.message.chat.send_action(ChatAction.RECORD_AUDIO)
+    lang="ml"
+    tts = gTTS(reply, lang)
+    tts.save("k.mp3")
+    with open("k.mp3", "rb") as f:
+        linelist = list(f)
+        linecount = len(linelist)
+    if linecount == 1:
+        update.message.chat.send_action(ChatAction.RECORD_AUDIO)
+        lang = "en"
+        tts = gTTS(reply, lang)
+        tts.save("k.mp3")
+    with open("k.mp3", "rb") as speech:
+        update.message.reply_voice(speech, quote=False)
+
+__help__ = """ Text to speech
+- /tts <your text>
+"""
+__mod_name__ = "TEXT TO SPEECH"
+
+dispatcher.add_handler(CommandHandler('tts', tts, pass_args=True))
