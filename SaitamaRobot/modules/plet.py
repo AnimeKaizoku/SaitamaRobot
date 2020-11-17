1c2d22bc67af63c2b13639af9c06d871244651e6
Uday0011 <73984830+Uday0011@users.noreply.github.com>
2020-11-17 23:54:02 +0530

    Update plet.py

--- SaitamaRobot/modules/plet.py
+++ SaitamaRobot/modules/plet.py
@@ -55,12 +55,16 @@ def plet(update: Update, context: CallbackContext):
         image.save(buffer, 'PNG')
         buffer.seek(0)
         context.bot.send_sticker(chat_id=message.chat_id, sticker=buffer)
+        
+PLET_HANDLER = DisableAbleCommandHandler("plet", plet)
 
+dispatcher.add_handler(PLET_HANDLER)
 
 __help__ = """
    /Plet :- text get funny emojify
 """
-__mod_name__ = "plet"
+__mod_name__ = "Plet"
 
-PLET_HANDLER = DisableAbleCommandHandler("plet", plet, admin_ok=True)
-dispatcher.add_handler(PLET_HANDLER)
+__handlers__ = [
+    PLET_HANDLER
+]
