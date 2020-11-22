bad17157872d1816119ea70d88eef5d0fc6e5968
akenohime <74871438+akenohime@users.noreply.github.com>
2020-11-23 00:04:42 +0700

    Update_main_.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -201,7 +201,7 @@ def start(update: Update, context: CallbackContext):
                 reply_markup=InlineKeyboardMarkup(
                     [[
                         InlineKeyboardButton(
-                            text="☑️ Add Saitama to your group",
+                            text="☑️ Add Me to your group",
                             url="t.me/{}?startgroup=true".format(
                                 context.bot.username))
                     ],
