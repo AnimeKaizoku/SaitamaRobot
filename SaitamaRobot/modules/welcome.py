92beaadee04b60967b857425b433b1b6950a0674
IronMan <73984830+Uday0011@users.noreply.github.com>
2020-11-22 23:25:41 +0530

    Update welcome.py

--- SaitamaRobot/modules/welcome.py
+++ SaitamaRobot/modules/welcome.py
@@ -181,7 +181,7 @@ def new_member(update: Update, context: CallbackContext):
             # Welcome Devs
             elif new_mem.id in DEV_USERS:
                 update.effective_message.reply_text(
-                    "Whoa! A member of the Heroes Association just joined!",
+                    "Whoa! A member of the Bot Lab just joined!",
                     reply_to_message_id=reply,
                 )
                 continue
