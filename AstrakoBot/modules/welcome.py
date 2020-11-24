3ddada86a950669ae12695a021a61fb9afba0c14
Alejandro <apamarin@gmail.com>
2020-11-24 20:18:43 +0100

    welcome: back to old module name again

--- AstrakoBot/modules/welcome.py
+++ AstrakoBot/modules/welcome.py
@@ -1050,7 +1050,7 @@ dispatcher.add_handler(CLEAN_SERVICE_HANDLER)
 dispatcher.add_handler(BUTTON_VERIFY_HANDLER)
 dispatcher.add_handler(WELCOME_MUTE_HELP)
 
-__mod_name__ = "Greetings"
+__mod_name__ = "Welcomes/Goodbyes"
 __command_list__ = []
 __handlers__ = [
     NEW_MEM_HANDLER,
