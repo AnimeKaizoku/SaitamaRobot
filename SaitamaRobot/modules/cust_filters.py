07c395852db83d9c26290f582a4a038ea1551967
Sawada Tsunayoshi <animekaizokux@gmail.com>
2020-11-24 17:42:45 +0530

    Added random replies help for notes and filters

--- SaitamaRobot/modules/cust_filters.py
+++ SaitamaRobot/modules/cust_filters.py
@@ -578,6 +578,14 @@ __help__ = """
 is mentioned. If you reply to a sticker with a keyword, the bot will reply with that sticker. NOTE: all filter \
 keywords are in lowercase. If you want your keyword to be a sentence, use quotes. eg: /filter "hey there" How you \
 doin?
+ Separate diff replies by `%%%` to get random replies
+ *Example:* 
+ `/filter "filtername"
+ Reply 1
+ %%%
+ Reply 2
+ %%%
+ Reply 3`
  • `/stop <filter keyword>`*:* Stop that filter.
 
 *Chat creator only:*
