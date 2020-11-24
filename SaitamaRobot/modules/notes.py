4253bc881e7d340108d7c5a16c3607d5190d88f9
DragSama <49992845+DragSama@users.noreply.github.com>
2020-11-24 11:21:33 +0530

    Fix random reply not being sent

--- SaitamaRobot/modules/notes.py
+++ SaitamaRobot/modules/notes.py
@@ -105,7 +105,7 @@ def get(update, context, notename, show_none=True, no_format=False):
                         text = valid_format
                 else:
                     text = valid_format
-                text = valid_format.format(
+                text = text.format(
                     first=escape_markdown(message.from_user.first_name),
                     last=escape_markdown(message.from_user.last_name or
                                          message.from_user.first_name),
