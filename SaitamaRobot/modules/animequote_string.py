479644110b8e524ec661a20dc7dfacb56e146635
JayPatel1314 <69890535+JayPatel1314@users.noreply.github.com>
2020-11-26 16:46:50 +0530

    Update animequote_string.py

--- SaitamaRobot/modules/animequote_string.py
+++ SaitamaRobot/modules/animequote_string.py
@@ -1,4 +1,4 @@
-#Made By @uday_gondaliya @AST_JayPatel1304
+#Made By @ironman1821 @AST_JayPatel1304
 ANIMEQUOTE = (
     "There are days when nothing goes right. There are days when you stumble and fall. There are days when you just want to cry. To cry a lot. To sleep a lot. Or even eat a lot. It’s alright, as long as you pick yourself up again. – Yuki Takeya (Gakkougurashi!)"    
     "Excuse me but are you by any chance the oddball in your family?– Ash Ketchum",
