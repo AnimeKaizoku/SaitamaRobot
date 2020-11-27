8f04638e9193cc72ead5792c720b20d563b91a05
Alejandro <apamarin@gmail.com>
2020-11-27 11:42:48 +0100

    github: give same commands format as other modules

--- AstrakoBot/modules/github.py
+++ AstrakoBot/modules/github.py
@@ -170,18 +170,18 @@ def getVer(update: Update, context: CallbackContext):
     return
 
 __help__ = """
-Github module. This module will fetch github releases.
-Commands:
- - /git <user>/<repo>: will fetch the most recent release from that repo.
- - /git <user>/<repo> <number>: will fetch releases in past.
- - /fetch <reponame> or &reponame: same as /git, but you can use a saved repo shortcut
- - /listrepo: lists all repo shortcuts in chat
- - /gitver: returns the current API version
- - /changelog <reponame>: gets the changelog of a saved repo shortcut
+*Github module. This module will fetch github releases*\n
+*Available commands:*
+ • `/git <user>/<repo>`: will fetch the most recent release from that repo.
+ • `/git <user>/<repo> <number>`: will fetch releases in past.
+ • `/fetch <reponame> or &reponame`: same as `/git`, but you can use a saved repo shortcut
+ • `/listrepo`: lists all repo shortcuts in chat
+ • `/gitver`: returns the current API version
+ • `/changelog <reponame>`: gets the changelog of a saved repo shortcut
  
-Admin only:
- - /saverepo <name> <user>/<repo> <number (optional)>: saves a repo value as shortcut
- - /delrepo <name>: deletes a repo shortcut
+*Admin only:*
+ • `/saverepo <name> <user>/<repo> <number (optional)>`: saves a repo value as shortcut
+ • `/delrepo <name>`: deletes a repo shortcut
 """
 
 __mod_name__ = "GitHub"
