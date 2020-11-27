8dfdf6217d32228a7d27b4fad858544bbb0503c1
Alejandro <apamarin@gmail.com>
2020-11-27 12:01:28 +0100

    backups: give same commands format as other modules

--- AstrakoBot/modules/backups.py
+++ AstrakoBot/modules/backups.py
@@ -368,10 +368,10 @@ __mod_name__ = "Backups"
 __help__ = """
 *Only for group owner:*
 
- • /import: Reply to the backup file for the butler / emilia group to import as much as possible, making transfers very easy! \
+ • `/import`: Reply to the backup file for the butler / emilia group to import as much as possible, making transfers very easy! \
  Note that files / photos cannot be imported due to telegram restrictions.
 
- • /export: Export group data, which will be exported are: rules, notes (documents, images, music, video, audio, voice, text, text buttons) \
+ • `/export`: Export group data, which will be exported are: rules, notes (documents, images, music, video, audio, voice, text, text buttons) \
 
 """
 
