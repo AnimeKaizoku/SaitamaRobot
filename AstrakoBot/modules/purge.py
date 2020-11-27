7aad8c8476643a32adc23503ed310382f95b8e9d
Alejandro <apamarin@gmail.com>
2020-11-27 11:44:57 +0100

    purge: give same commands format as other modules

--- AstrakoBot/modules/purge.py
+++ AstrakoBot/modules/purge.py
@@ -71,9 +71,9 @@ async def delete_messages(event):
 
 __help__ = """
 *Admin only:*
- - /del: deletes the message you replied to
- - /purge: deletes all messages between this and the replied to message.
- - /purge <integer X>: deletes the replied message, and X messages following it if replied to a message.
+ • `/del`: deletes the message you replied to
+ • `/purge`: deletes all messages between this and the replied to message.
+ • `/purge <integer X>`: deletes the replied message, and X messages following it if replied to a message.
 """
 
 __mod_name__ = "Purges"
