4a929b58e3ed06d935d9ebb97f88372ab47506a5
IronMan <73984830+Uday0011@users.noreply.github.com>
2020-11-28 23:58:07 +0530

    Update reverse.py

--- SaitamaRobot/modules/reverse.py
+++ SaitamaRobot/modules/reverse.py
@@ -180,6 +180,11 @@ def scam(imgspage, lim):
 
     return imglinks
 
+__help__ = """
+   /Reverse :- Please reply to a sticker, or an image to search it!
+Do you know that you can search an image with a link too? /reverse picturelink <amount>.
+"""
+__mod_name__ = "Reverse"
 
 REVERSE_HANDLER = DisableAbleCommandHandler(
     "reverse", reverse, pass_args=True, admin_ok=True
