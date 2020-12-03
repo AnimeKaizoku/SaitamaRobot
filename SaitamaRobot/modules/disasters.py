0e84a2d6a0636c5c23870d9ea577e6f5abba0156
Sawada Tsunayoshi <animekaizokux@gmail.com>
2020-12-03 18:31:56 +0530

    [disasters] Documented leave command

--- SaitamaRobot/modules/disasters.py
+++ SaitamaRobot/modules/disasters.py
@@ -547,6 +547,7 @@ Group admins/group owners do not need these commands.
 
  ╔ *Groups Info:*
  ╠ `/groups`*:* List the groups with Name, ID, members count as a txt
+ ╠ `/leave <ID>`*:* Leave the group, ID must have hyphen
  ╚ `/getchats`*:* Gets a list of group names the user has been seen in. Bot owner only
 
  ╔ *Blacklist:* 
