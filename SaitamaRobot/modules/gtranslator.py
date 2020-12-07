e29ae03fd29b670f8f699d1c2a17a66601fed013
IronMan <73984830+Uday0011@users.noreply.github.com>
2020-12-07 10:33:04 +0530

    Update gtranslator.py

--- SaitamaRobot/modules/gtranslator.py
+++ SaitamaRobot/modules/gtranslator.py
@@ -1,115 +1,76 @@
 from emoji import UNICODE_EMOJI
-from googletrans import LANGUAGES, Translator
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from google_trans_new import LANGUAGES, google_translator
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
+from SaitamaRobot import dispatcher
+from SaitamaRobot.modules.disable import DisableAbleCommandHandler
 
 @run_async
 def totranslate(update: Update, context: CallbackContext):
-    msg = update.effective_message
+    message = update.effective_message
     problem_lang_code = []
     for key in LANGUAGES:
         if "-" in key:
             problem_lang_code.append(key)
-    try:
-        if msg.reply_to_message:
-            args = update.effective_message.text.split(None, 1)
-            if msg.reply_to_message.text:
-                text = msg.reply_to_message.text
-            elif msg.reply_to_message.caption:
-                text = msg.reply_to_message.caption
 
-            message = update.effective_message
-            dest_lang = None
+    try:
+        if message.reply_to_message:
+            args = update.effective_message.text.split(None, 1)
+            if message.reply_to_message.text:
+                text = message.reply_to_message.text
+            elif message.reply_to_message.caption:
+                text = message.reply_to_message.caption
 
             try:
                 source_lang = args[1].split(None, 1)[0]
-            except:
+            except (IndexError, AttributeError):
                 source_lang = "en"
 
-            if source_lang.count('-') == 2:
-                for lang in problem_lang_code:
-                    if lang in source_lang:
-                        if source_lang.startswith(lang):
-                            dest_lang = source_lang.rsplit("-", 1)[1]
-                            source_lang = source_lang.rsplit("-", 1)[0]
-                        else:
-                            dest_lang = source_lang.split("-", 1)[1]
-                            source_lang = source_lang.split("-", 1)[0]
-            elif source_lang.count('-') == 1:
-                for lang in problem_lang_code:
-                    if lang in source_lang:
-                        dest_lang = source_lang
-                        source_lang = None
-                        break
-                if dest_lang is None:
-                    dest_lang = source_lang.split("-")[1]
-                    source_lang = source_lang.split("-")[0]
-            else:
-                dest_lang = source_lang
-                source_lang = None
-
-            exclude_list = UNICODE_EMOJI.keys()
-            for emoji in exclude_list:
-                if emoji in text:
-                    text = text.replace(emoji, '')
-
-            trl = Translator()
-            if source_lang is None:
-                detection = trl.detect(text)
-                tekstr = trl.translate(text, dest=dest_lang)
-                return message.reply_text(
-                    f"Translated from `{detection.lang}` to `{dest_lang}`:\n`{tekstr.text}`",
-                    parse_mode=ParseMode.MARKDOWN)
-            else:
-                tekstr = trl.translate(text, dest=dest_lang, src=source_lang)
-                message.reply_text(
-                    f"Translated from `{source_lang}` to `{dest_lang}`:\n`{tekstr.text}`",
-                    parse_mode=ParseMode.MARKDOWN)
         else:
             args = update.effective_message.text.split(None, 2)
-            message = update.effective_message
-            source_lang = args[1]
             text = args[2]
-            exclude_list = UNICODE_EMOJI.keys()
-            for emoji in exclude_list:
-                if emoji in text:
-                    text = text.replace(emoji, '')
-            dest_lang = None
-            temp_source_lang = source_lang
-            if temp_source_lang.count('-') == 2:
-                for lang in problem_lang_code:
-                    if lang in temp_source_lang:
-                        if temp_source_lang.startswith(lang):
-                            dest_lang = temp_source_lang.rsplit("-", 1)[1]
-                            source_lang = temp_source_lang.rsplit("-", 1)[0]
-                        else:
-                            dest_lang = temp_source_lang.split("-", 1)[1]
-                            source_lang = temp_source_lang.split("-", 1)[0]
-            elif temp_source_lang.count('-') == 1:
-                for lang in problem_lang_code:
-                    if lang in temp_source_lang:
-                        dest_lang = None
-                        break
+            source_lang = args[1]
+
+        if source_lang.count('-') == 2:
+            for lang in problem_lang_code:
+                if lang in source_lang:
+                    if source_lang.startswith(lang):
+                        dest_lang = source_lang.rsplit("-", 1)[1]
+                        source_lang = source_lang.rsplit("-", 1)[0]
                     else:
-                        dest_lang = temp_source_lang.split("-")[1]
-                        source_lang = temp_source_lang.split("-")[0]
-            trl = Translator()
+                        dest_lang = source_lang.split("-", 1)[1]
+                        source_lang = source_lang.split("-", 1)[0]
+        elif source_lang.count('-') == 1:
+            for lang in problem_lang_code:
+                if lang in source_lang:
+                    dest_lang = source_lang
+                    source_lang = None
+                    break
             if dest_lang is None:
-                detection = trl.detect(text)
-                tekstr = trl.translate(text, dest=source_lang)
-                return message.reply_text(
-                    "Translated from `{}` to `{}`:\n`{}`".format(
-                        detection.lang, source_lang, tekstr.text),
-                    parse_mode=ParseMode.MARKDOWN)
-            else:
-                tekstr = trl.translate(text, dest=dest_lang, src=source_lang)
-                message.reply_text(
-                    "Translated from `{}` to `{}`:\n`{}`".format(
-                        source_lang, dest_lang, tekstr.text),
-                    parse_mode=ParseMode.MARKDOWN)
+                dest_lang = source_lang.split("-")[1]
+                source_lang = source_lang.split("-")[0]
+        else:
+            dest_lang = source_lang
+            source_lang = None
+
+        exclude_list = UNICODE_EMOJI.keys()
+        for emoji in exclude_list:
+            if emoji in text:
+                text = text.replace(emoji, '')
+
+        trl = google_translator()
+        if source_lang is None:
+            detection = trl.detect(text)
+            trans_str = trl.translate(text, lang_tgt=dest_lang)
+            return message.reply_text(
+                f"Translated from `{detection[0]}` to `{dest_lang}`:\n`{trans_str}`",
+                parse_mode=ParseMode.MARKDOWN)
+        else:
+            trans_str = trl.translate(text, lang_tgt=dest_lang, lang_src=source_lang)
+            message.reply_text(
+                f"Translated from `{source_lang}` to `{dest_lang}`:\n`{trans_str}`",
+                parse_mode=ParseMode.MARKDOWN)
 
     except IndexError:
         update.effective_message.reply_text(
