699c4ece202c8ba82dfe8e21e661dae572c503fa
Dark Samurai <73326139+ninja7-bot@users.noreply.github.com>
2020-12-08 21:03:45 +0530

    Update games.py

--- SaitamaRobot/modules/games.py
+++ SaitamaRobot/modules/games.py
@@ -27,28 +27,32 @@ def dare(update: Update, context: CallbackContext):
 def tord(update: Update, context: CallbackContext):
     update.effective_message.reply_text(random.choice(fun_strings.TORD_STRINGS))
 
+@run_async
+def wyr(update: Update, context: CallbackContext):
+    update.effective_message.reply_text(random.choice(fun_strings.WYR_STRINGS))
 
 __help__ = """
  • `/truth`*:* asks you a question
  • `/dare`*:* gives you a dare
  • `/tord`*:* can be a truth or a dare
-"""
+ • `/wyr`*:* would you rather
+  """
 
 TRUTH_HANDLER = DisableAbleCommandHandler("truth", truth)
 DARE_HANDLER = DisableAbleCommandHandler("dare", dare)
 TORD_HANDLER = DisableAbleCommandHandler("tord", tord)
-
+WYR_HANDLER = DisableAbleCommandHandler("wyr", tord)
 
 dispatcher.add_handler(TRUTH_HANDLER)
 dispatcher.add_handler(DARE_HANDLER)
 dispatcher.add_handler(TORD_HANDLER)
-
+dispatcher.add_handler(WYR_HANDLER)
 
 __mod_name__ = "Games"
 __command_list__ = [
-   "truth", "dare", "tord",
+   "truth", "dare", "tord", "wyr"
 ]
 
 __handlers__ = [
-    TRUTH_HANDLER, DARE_HANDLER, TORD_HANDLER,
+    TRUTH_HANDLER, DARE_HANDLER, TORD_HANDLER, WYR_HANDLER
 ]
