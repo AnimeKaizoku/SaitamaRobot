7295e152e4a813609e310fe15ace1c15fb8a506e
Dark Samurai <73326139+ninja7-bot@users.noreply.github.com>
2020-12-08 19:58:50 +0530

    Update games.py

--- SaitamaRobot/modules/games.py
+++ SaitamaRobot/modules/games.py
@@ -27,7 +27,13 @@ def dare(update: Update, context: CallbackContext):
 def tord(update: Update, context: CallbackContext):
     update.effective_message.reply_text(random.choice(fun_strings.TORD_STRINGS))
 
-
+"""
+ __help__ = 
+ • `/truth`*:* asks you a question
+ • `/dare`*:* gives you a dare
+ • `/tord`*:* can be a truth or a dare
+ 
+ """
 
 TRUTH_HANDLER = DisableAbleCommandHandler("truth", truth)
 DARE_HANDLER = DisableAbleCommandHandler("dare", dare)
