e4e453583f802e26f208819defb2eafade3cf749
Dark Samurai <73326139+ninja7-bot@users.noreply.github.com>
2020-12-20 10:08:23 +0530

    Update lyrics.py

--- SaitamaRobot/modules/lyrics.py
+++ SaitamaRobot/modules/lyrics.py
@@ -44,6 +44,8 @@ Want to get the lyrics of your favorite songs straight from the app? This module
  You can either enter just the song name or both the artist and song name.
 """
 
+__mod_name__ = "Lyrics"
+
 LYRICS_HANDLER = DisableAbleCommandHandler("lyrics", lyrics, pass_args=True)
 
 dispatcher.add_handler(LYRICS_HANDLER)
