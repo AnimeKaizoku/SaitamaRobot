a9895156c7f79612c3b2599a5a47f7477cb899a8
Chizuru <chizuru@kanojo.tk>
2020-12-22 22:42:06 +0530

    [init] Handle SpamWatch connection error.
    Handles the super annoying ConnectionError that occurs sometimes during bot startup.

--- SaitamaRobot/__init__.py
+++ SaitamaRobot/__init__.py
@@ -171,7 +171,12 @@ if not SPAMWATCH_API:
     sw = None
     LOGGER.warning("SpamWatch API key missing! recheck your config.")
 else:
-    sw = spamwatch.Client(SPAMWATCH_API)
+    try:
+        sw = spamwatch.Client(SPAMWATCH_API)
+    except:
+        sw = None
+        LOGGER.warning("Can't connect to SpamWatch!")
+
 
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
 telethn = TelegramClient("saitama", API_ID, API_HASH)
