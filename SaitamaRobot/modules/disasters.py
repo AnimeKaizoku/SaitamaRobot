bc1a4122f1ec3f109151bdb0787bdc971a57fc02
DRAGON SAMA <51018664+MrBhavya@users.noreply.github.com>
2020-12-22 05:17:08 +0000

    Update disasters.py

--- SaitamaRobot/modules/disasters.py
+++ SaitamaRobot/modules/disasters.py
@@ -528,14 +528,83 @@ def devlist(update: Update, context: CallbackContext):
 
 
 __help__ = f"""
- • `/heroes`*:* Lists all Hero Association members.
- • `/dragons`*:* Lists all Dragon disasters.
- • `/demons`*:* Lists all Demon disasters.
- • `/tigers`*:* Lists all Tigers disasters.
- • `/wolves`*:* Lists all Wolf disasters.
- *Note:* These commands list users with special bot priveleges and can only be used by them.
- You can visit {SUPPORT_CHAT} to query more about these.
+*⚠️ Notice:*
+Commands listed here only work for users with special access are mainly used for troubleshooting, debugging purposes.
+Group admins/group owners do not need these commands. 
+ ╔ *List all special users:*
+ ╠ `/dragons`*:* Lists all Dragon disasters
+ ╠ `/demons`*:* Lists all Demon disasters
+ ╠ `/tigers`*:* Lists all Tigers disasters
+ ╠ `/wolves`*:* Lists all Wolf disasters
+ ╠ `/heroes`*:* Lists all Hero Association members
+ ╠ `/adddragon`*:* Adds a user to Dragon
+ ╠ `/adddemon`*:* Adds a user to Demon
+ ╠ `/addtiger`*:* Adds a user to Tiger
+ ╠ `/addwolf`*:* Adds a user to Wolf
+ ╚ `Add dev doesnt exist, devs should know how to add themselves`
+ ╔ *Ping:*
+ ╠ `/ping`*:* gets ping time of bot to telegram server
+ ╚ `/pingall`*:* gets all listed ping times
+ ╔ *Broadcast: (Bot owner only)*
+ ╠  *Note:* This supports basic markdown
+ ╠ `/broadcastall`*:* Broadcasts everywhere
+ ╠ `/broadcastusers`*:* Broadcasts too all users
+ ╚ `/broadcastgroups`*:* Broadcasts too all groups
+ ╔ *Groups Info:*
+ ╠ `/groups`*:* List the groups with Name, ID, members count as a txt
+ ╠ `/leave <ID>`*:* Leave the group, ID must have hyphen
+ ╠ `/stats`*:* Shows overall bot stats
+ ╠ `/getchats`*:* Gets a list of group names the user has been seen in. Bot owner only
+ ╚ `/ginfo username/link/ID`*:* Pulls info panel for entire group
+ ╔ *Access control:* 
+ ╠ `/ignore`*:* Blacklists a user from 
+ ╠  using the bot entirely
+ ╠ `/notice`*:* Removes user from blacklist
+ ╚ `/ignoredlist`*:* Lists ignored users
+ ╔ *Module loading:*
+ ╠ `/listmodules`*:* Prints modules and their names
+ ╠ `/unload <name>`*:* Unloads module dynamically
+ ╚ `/load <name>`*:* Loads module
+ ╔ *Speedtest:*
+ ╚ `/speedtest`*:* Runs a speedtest and gives you 2 options to choose from, text or image output
+ ╔ *Global Bans:*
+ ╠ `/gban user reason`*:* Globally bans a user
+ ╚ `/ungban user reason`*:* Unbans the user from the global bans list
+ ╔ *Module loading:*
+ ╠ `/listmodules`*:* Lists names of all modules
+ ╠ `/load modulename`*:* Loads the said module to 
+ ╠   memory without restarting.
+ ╠ `/unload modulename`*:* Loads the said module from
+ ╚   memory without restarting.memory without restarting the bot 
+ ╔ *Remote commands:*
+ ╠ `/rban user group`*:* Remote ban
+ ╠ `/runban user group`*:* Remote un-ban
+ ╠ `/rpunch user group`*:* Remote punch
+ ╠ `/rmute user group`*:* Remote mute
+ ╚ `/runmute user group`*:* Remote un-mute
+ ╔ *Windows self hosted only:*
+ ╠ `/reboot`*:* Restarts the bots service
+ ╚ `/gitpull`*:* Pulls the repo and then restarts the bots service
+ ╔ *Chatbot:* 
+ ╚ `/listaichats`*:* Lists the chats the chatmode is enabled in
+ 
+ ╔ *Debugging and Shell:* 
+ ╠ `/debug <on/off>`*:* Logs commands to updates.txt
+ ╠ `/logs`*:* Run this in support group to get logs in pm
+ ╠ `/eval`*:* Self explanatory
+ ╠ `/sh`*:* Runs shell command
+ ╠ `/shell`*:* Runs shell command
+ ╠ `/clearlocals`*:* As the name goes
+ ╠ `/dbcleanup`*:* Removes deleted accs and groups from db
+ ╚ `/py`*:* Runs python code
+ 
+ ╔ *Global Bans:*
+ ╠ `/gban <id> <reason>`*:* Gbans the user, works by reply too
+ ╠ `/ungban`*:* Ungbans the user, same usage as gban
+ ╚ `/gbanlist`*:* Outputs a list of gbanned users
+Visit @{SUPPORT_CHAT} for more information.
 """
+
 
 SUDO_HANDLER = CommandHandler(("addsudo", "adddragon"), addsudo)
 SUPPORT_HANDLER = CommandHandler(("addsupport", "adddemon"), addsupport)
