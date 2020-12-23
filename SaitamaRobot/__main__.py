79783ccb1f903dbc5c1facb539ba14748a7da353
IAmKartoon <72184549+IAmKartoon@users.noreply.github.com>
2020-12-23 07:14:14 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -51,15 +51,15 @@ def get_readable_time(seconds: int) -> str:
 
 
 PM_START_TEXT = """
-Hi {}, my name is {}! 
-I am an Anime themed group management bot.
-Build by weebs for weebs, I specialize in managing anime and similar themed groups.
+Why ping me up while I'm asleep you little brat! 
+Well if you had, just listen now!
+My name is Karma Akabane! Build by weebs for weebs, I specialize in managing anime and similar themed groups.
 You can find my list of available commands with /help.
 """
 
 HELP_STRINGS = """
-Hey there! My name is *{}*.
-I'm a Hero For Fun and help admins manage their groups with One Punch! Have a look at the following for an idea of some of \
+Hey there! My name is Karma Akabane.
+Have a look at the following for an idea of some of \
 the things I can help you with.
 
 *Main* commands available:
