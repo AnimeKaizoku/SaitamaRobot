93ad4a4051a6d7b47d54d5c0b518c74bba51a29b
Tanjiro Kamado <69890535+JayPatel1314@users.noreply.github.com>
2021-01-21 11:50:15 +0530

    Create imdb.py

---
+++ SaitamaRobot/modules/imdb.py
@@ -0,0 +1,111 @@
+import bs4
+import requests
+import re
+from SaitamaRobot.events import register
+from SaitamaRobot import telethn
+from telethon import types
+from telethon.tl import functions
+
+langi = "en"
+
+async def is_register_admin(chat, user):
+    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
+
+        return isinstance(
+            (await telethn(functions.channels.GetParticipantRequest(chat, user))).participant,
+            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator)
+        )
+    if isinstance(chat, types.InputPeerChat):
+
+        ui = await telethn.get_peer_id(user)
+        ps = (await telethn(functions.messages.GetFullChatRequest(chat.chat_id))) \
+                .full_chat.participants.participants
+        return isinstance(
+            next((p for p in ps if p.user_id == ui), None),
+            (types.ChatParticipantAdmin, types.ChatParticipantCreator)
+        )
+    return None
+
+@register(pattern="^/imdb (.*)") 
+async def imdb(e):
+ if e.is_group:
+  if not (await is_register_admin(e.input_chat, e.message.sender_id)):
+     await event.reply(" You are not admin. You can't use this command.. But you can use in my pm")
+     return
+ try:
+    movie_name = e.pattern_match.group(1)
+    remove_space = movie_name.split(' ')
+    final_name = '+'.join(remove_space)
+    page = requests.get("https://www.imdb.com/find?ref_=nv_sr_fn&q="+final_name+"&s=all")
+    lnk = str(page.status_code)
+    soup = bs4.BeautifulSoup(page.content,'lxml')
+    odds = soup.findAll("tr","odd")
+    mov_title = odds[0].findNext('td').findNext('td').text
+    mov_link = "http://www.imdb.com/"+odds[0].findNext('td').findNext('td').a['href']
+    page1 = requests.get(mov_link)
+    soup = bs4.BeautifulSoup(page1.content,'lxml')
+    if soup.find('div','poster'):
+    	poster = soup.find('div','poster').img['src']
+    else:
+    	poster = ''
+    if soup.find('div','title_wrapper'):
+    	pg = soup.find('div','title_wrapper').findNext('div').text
+    	mov_details = re.sub(r'\s+',' ',pg)
+    else:
+    	mov_details = ''
+    credits = soup.findAll('div', 'credit_summary_item')
+    if len(credits)==1:
+    	director = credits[0].a.text
+    	writer = 'Not available'
+    	stars = 'Not available'
+    elif len(credits)>2:
+    	director = credits[0].a.text
+    	writer = credits[1].a.text
+    	actors = []
+    	for x in credits[2].findAll('a'):
+    		actors.append(x.text)
+    	actors.pop()
+    	stars = actors[0]+','+actors[1]+','+actors[2]
+    else:
+    	director = credits[0].a.text
+    	writer = 'Not available'
+    	actors = []
+    	for x in credits[1].findAll('a'):
+    		actors.append(x.text)
+    	actors.pop()
+    	stars = actors[0]+','+actors[1]+','+actors[2]
+    if soup.find('div', "inline canwrap"):
+    	story_line = soup.find('div', "inline canwrap").findAll('p')[0].text
+    else:
+    	story_line = 'Not available'
+    info = soup.findAll('div', "txt-block")
+    if info:
+    	mov_country = []
+    	mov_language = []
+    	for node in info:
+    		a = node.findAll('a')
+    		for i in a:
+    			if "country_of_origin" in i['href']:
+    				mov_country.append(i.text)
+    			elif "primary_language" in i['href']:
+    				mov_language.append(i.text)
+    if soup.findAll('div',"ratingValue"):
+    	for r in soup.findAll('div',"ratingValue"):
+    		mov_rating = r.strong['title']
+    else:
+    	mov_rating = 'Not available'
+    await e.reply('<a href='+poster+'>&#8203;</a>'
+    			'<b>Title : </b><code>'+mov_title+
+    			'</code>\n<code>'+mov_details+
+    			'</code>\n<b>Rating : </b><code>'+mov_rating+
+    			'</code>\n<b>Country : </b><code>'+mov_country[0]+
+    			'</code>\n<b>Language : </b><code>'+mov_language[0]+
+    			'</code>\n<b>Director : </b><code>'+director+
+    			'</code>\n<b>Writer : </b><code>'+writer+
+    			'</code>\n<b>Stars : </b><code>'+stars+
+    			'</code>\n<b>IMDB Url : </b>'+mov_link+
+    			'\n<b>Story Line : </b>'+story_line,
+    			link_preview = True , parse_mode = 'HTML'
+    			)
+ except IndexError:
+     await e.reply("Please enter a valid movie name !")
