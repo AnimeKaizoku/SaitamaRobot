bbd9d10e604368ddc87fea98e7191168bfaa8d40
IAmKartoon <72184549+IAmKartoon@users.noreply.github.com>
2021-01-23 12:52:48 +0530

    Create truth_and_dare.py

---
+++ SaitamaRobot/modules/truth_and_dare.py
@@ -0,0 +1,23 @@
+import html
+import random
+import SaitamaRobot.modules.truth_and_dare_string as truth_and_dare_string
+from SaitamaRobot import dispatcher
+from telegram import ParseMode, Update, Bot
+from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from telegram.ext import CallbackContext, run_async
+
+@run_async
+def truth(update: Update, context: CallbackContext):
+    args = context.args
+    update.effective_message.reply_text(random.choice(truth_and_dare_string.TRUTH))
+
+@run_async
+def dare(update: Update, context: CallbackContext):
+    args = context.args
+    update.effective_message.reply_text(random.choice(truth_and_dare_string.DARE))
+
+TRUTH_HANDLER = DisableAbleCommandHandler("truth", truth)
+DARE_HANDLER = DisableAbleCommandHandler("dare", dare)
+
+dispatcher.add_handler(TRUTH_HANDLER)
+dispatcher.add_handler(DARE_HANDLER)
