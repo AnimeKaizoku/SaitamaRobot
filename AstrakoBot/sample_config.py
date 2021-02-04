b2f39145a3ecdaffc2c3b6794cd48bd826096ebb
astrako <apamarin@gmail.com>
2021-02-04 15:40:39 +0100

    sample_config: replace some vars with more generic values

--- AstrakoBot/sample_config.py
+++ AstrakoBot/sample_config.py
@@ -17,14 +17,14 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Sawada"
-    SUPPORT_CHAT = "AstrakoBotSupport"  # Your own group for support, do not add the @
+    OWNER_ID = 123456789  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "awoo"
+    SUPPORT_CHAT = "awoo"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001253661229
+        -1234567890123
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001190806654
+        -1234567890123
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
     ALLOW_CHATS = True
 
