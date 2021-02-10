ad7c5c2b0a8770aa83b8f0064de7535deeda370e
Eren Yeager <78803287+OfficialErenYeager@users.noreply.github.com>
2021-02-10 10:06:16 +0500

    Made some changes to Pm and Help

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -74,8 +74,8 @@ def get_readable_time(seconds: int) -> str:
 
 PM_START_TEXT = """
 Hi {}, my name is {}! 
-I am an Anime themed group management bot.
-Build by weebs for weebs, I specialize in managing anime and similar themed groups.
+I am Attack Titan group management bot.
+My owner is Eren Yeager, I specialize in managing anime and to kill Titans.
 You can find my list of available commands with /help.
 """
 
