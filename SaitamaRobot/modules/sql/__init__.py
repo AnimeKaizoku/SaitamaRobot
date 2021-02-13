1e0deb13a0f11ab9471d0bcf61d21381e1965297
Sawada Tsunayoshi <animekaizokux@gmail.com>
2021-02-13 18:31:36 +0530

    Corrected a typo in a var

--- SaitamaRobot/modules/sql/__init__.py
+++ SaitamaRobot/modules/sql/__init__.py
@@ -1,11 +1,11 @@
-from SaitamaRobot import DB_URI
+from SaitamaRobot import DB_URL
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 
 def start() -> scoped_session:
-    engine = create_engine(DB_URI, client_encoding="utf8")
+    engine = create_engine(DB_URL, client_encoding="utf8")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
