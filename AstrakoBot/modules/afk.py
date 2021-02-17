8124c3ce070f4e5bee66a0455d51968e8e0a4158
Roj Serbest <rojserbest@gmail.com>
2021-02-17 15:18:12 +0300

    afk: add type hints and correct a small typo

--- AstrakoBot/modules/afk.py
+++ AstrakoBot/modules/afk.py
@@ -1,4 +1,5 @@
-import random, html
+import random
+import html
 
 from AstrakoBot import dispatcher
 from AstrakoBot.modules.disable import (
@@ -37,7 +38,8 @@ def afk(update: Update, context: CallbackContext):
     sql.set_afk(update.effective_user.id, reason)
     fname = update.effective_user.first_name
     try:
-        update.effective_message.reply_text("{} is now away!{}".format(fname, notice))
+        update.effective_message.reply_text(
+            "{} is now away!{}".format(fname, notice))
     except BadRequest:
         pass
 
@@ -66,7 +68,8 @@ def no_longer_afk(update: Update, context: CallbackContext):
                 "Where is {}?\nIn the chat!",
             ]
             chosen_option = random.choice(options)
-            update.effective_message.reply_text(chosen_option.format(firstname))
+            update.effective_message.reply_text(
+                chosen_option.format(firstname))
         except:
             return
 
@@ -96,7 +99,8 @@ def reply_afk(update: Update, context: CallbackContext):
             if ent.type != MessageEntity.MENTION:
                 return
 
-            user_id = get_user_id(message.text[ent.offset : ent.offset + ent.length])
+            user_id = get_user_id(
+                message.text[ent.offset: ent.offset + ent.length])
             if not user_id:
                 # Should never happen, since for a user to become AFK they must have spoken. Maybe changed username?
                 return
@@ -120,7 +124,7 @@ def reply_afk(update: Update, context: CallbackContext):
         check_afk(update, context, user_id, fst_name, userc_id)
 
 
-def check_afk(update: Update, context: CallbackContext, user_id, fst_name, userc_id):
+def check_afk(update: Update, context: CallbackContext, user_id: int, fst_name: str, userc_id: int):
     if sql.is_afk(user_id):
         user = sql.check_afk_status(user_id)
         if int(userc_id) == int(user_id):
@@ -136,7 +140,7 @@ def check_afk(update: Update, context: CallbackContext, user_id, fst_name, userc
 
 
 __help__ = """
- • `/afk <reason>`*:* mark yourself as AFK(away from keyboard).
+ • `/afk <reason>`*:* mark yourself as AFK (away from keyboard).
  • `brb <reason>`*:* same as the afk command - but not a command.
 When marked as AFK, any mentions will be replied to with a message to say you're not available!
 """
