bfb47f709e7fff5cf763c60e0006098ca3ba906d
Astrako <apamarin@gmail.com>
2021-03-22 09:38:15 +0100

    android: misc modifications
    Reply a message if no device codename is provided for twrp and orangefox commands and remove ofox command

--- AstrakoBot/modules/android.py
+++ AstrakoBot/modules/android.py
@@ -41,43 +41,48 @@ def magisk(update: Update, context: CallbackContext):
 def orangefox(update: Update, context: CallbackContext):
     bot, args = context.bot, context.args
     message = update.effective_message
-    device = args[0]
-    link = get(f"https://api.orangefox.download/v3/releases/?codename={device}&sort=date_desc&limit=1")
+    device = message.text[len("/orangefox ") :]
 
-    if link.status_code == 404:
-        message = f"OrangeFox currently is not avaliable for {device}"
+    if device:
+        link = get(f"https://api.orangefox.download/v3/releases/?codename={device}&sort=date_desc&limit=1")
+
+        if link.status_code == 404:
+            message = f"OrangeFox currently is not avaliable for {device}"
+        else:
+            page = loads(link.content)
+            file_id = page["data"][0]["_id"]
+            link = get(f"https://api.orangefox.download/v3/devices/get?codename={device}")
+            page = loads(link.content)
+            oem = page["oem_name"]
+            model = page["model_name"]
+            full_name = page["full_name"]
+            link = get(f"https://api.orangefox.download/v3/releases/get?_id={file_id}")
+            page = loads(link.content)
+            dl_file = page["filename"]
+            build_type = page["type"]
+            version = page["version"]
+            changelog = page["changelog"][0]
+            size = page["size"]
+            dl_link = page["mirrors"]["DL"]
+            date = page["date"]
+            md5 = page["md5"]
+            message = f"<b>Latest OrangeFox Recovery for the {full_name}</b>\n\n"
+            message += f"• Manufacturer: {oem}\n"
+            message += f"• Model: {model}\n"
+            message += f"• Codename: {device}\n"
+            message += f"• Release type: official\n"
+            message += f"• Build type: {build_type}\n"
+            message += f"• Version: {version}\n"
+            message += f"• Changelog: {changelog}\n"
+            message += f"• Size: {size}\n"
+            message += f"• Date: {date}\n"
+            message += f"• File: {dl_file}\n"
+            message += f"• MD5: {md5}\n\n"
+            message += f"• <b>Download:</b> {dl_link}\n"        
 
     else:
-        page = loads(link.content)
-        file_id = page["data"][0]["_id"]
-        link = get(f"https://api.orangefox.download/v3/devices/get?codename={device}")
-        page = loads(link.content)
-        oem = page["oem_name"]
-        model = page["model_name"]
-        full_name = page["full_name"]
-        link = get(f"https://api.orangefox.download/v3/releases/get?_id={file_id}")
-        page = loads(link.content)
-        dl_file = page["filename"]
-        build_type = page["type"]
-        version = page["version"]
-        changelog = page["changelog"][0]
-        size = page["size"]
-        dl_link = page["mirrors"]["DL"]
-        date = page["date"]
-        md5 = page["md5"]
-        message = f"<b>Latest OrangeFox Recovery for the {full_name}</b>\n\n"
-        message += f"• Manufacturer: {oem}\n"
-        message += f"• Model: {model}\n"
-        message += f"• Codename: {device}\n"
-        message += f"• Release type: official\n"
-        message += f"• Build type: {build_type}\n"
-        message += f"• Version: {version}\n"
-        message += f"• Changelog: {changelog}\n"
-        message += f"• Size: {size}\n"
-        message += f"• Date: {date}\n"
-        message += f"• File: {dl_file}\n"
-        message += f"• MD5: {md5}\n\n"
-        message += f"• <b>Download:</b> {dl_link}\n"
+        message = "Please specify a device codename"
+
 
     bot.send_message(
         chat_id=update.effective_chat.id,
@@ -90,26 +95,29 @@ def orangefox(update: Update, context: CallbackContext):
 def twrp(update: Update, context: CallbackContext):
     bot, args = context.bot, context.args
     message = update.effective_message
-    device = args[0]
-    link = get(f"https://eu.dl.twrp.me/{device}")
+    device = message.text[len("/twrp ") :]
 
-    if link.status_code == 404:
-        message = f"TWRP currently is not avaliable for {device}"
+    if device:
+        link = get(f"https://eu.dl.twrp.me/{device}")
 
+        if link.status_code == 404:
+            message = f"TWRP currently is not avaliable for {device}"
+        else:
+            page = BeautifulSoup(link.content, "lxml")
+            download = page.find("table").find("tr").find("a")
+            dl_link = f"https://eu.dl.twrp.me{download['href']}"
+            dl_file = download.text
+            size = page.find("span", {"class": "filesize"}).text
+            date = page.find("em").text.strip()
+            message = f"<b>Latest TWRP for the {device}</b>\n\n"
+            message += f"• Release type: official\n"
+            message += f"• Size: {size}\n"
+            message += f"• Date: {date}\n"
+            message += f"• File: {dl_file}\n\n"
+            message += f"• <b>Download:</b> {dl_link}\n"
     else:
+        message = "Please specify a device codename"
 
-        page = BeautifulSoup(link.content, "lxml")
-        download = page.find("table").find("tr").find("a")
-        dl_link = f"https://eu.dl.twrp.me{download['href']}"
-        dl_file = download.text
-        size = page.find("span", {"class": "filesize"}).text
-        date = page.find("em").text.strip()
-        message = f"<b>Latest TWRP for the {device}</b>\n\n"
-        message += f"• Release type: official\n"
-        message += f"• Size: {size}\n"
-        message += f"• Date: {date}\n"
-        message += f"• File: {dl_file}\n\n"
-        message += f"• <b>Download:</b> {dl_link}\n"
 
     bot.send_message(
         chat_id=update.effective_chat.id,
@@ -124,12 +132,12 @@ __help__ = """
 *Magisk:* 
 • `/magisk`, `/su`, `/root`: fetches latest magisk\n
 *OrangeFox Recovery Project:* 
-• `/ofox`, `/orangefox` `<devicecodename>`: fetches lastest OrangeFox Recovery available for a given device codename\n
+• `/orangefox` `<devicecodename>`: fetches lastest OrangeFox Recovery available for a given device codename\n
 *TWRP:* 
 • `/twrp <devicecodename>`: fetches lastest TWRP available for a given device codename
 """
 magisk_handler = CommandHandler(["magisk", "root", "su"], magisk, run_async=True)
-orangefox_handler = CommandHandler(["ofox", "orangefox"], orangefox, run_async=True)
+orangefox_handler = CommandHandler("orangefox", orangefox, run_async=True)
 twrp_handler = CommandHandler("twrp", twrp, run_async=True)
 
 dispatcher.add_handler(magisk_handler)
@@ -137,5 +145,5 @@ dispatcher.add_handler(orangefox_handler)
 dispatcher.add_handler(twrp_handler)
 
 __mod_name__ = "Android"
-__command_list__ = ["magisk", "root", "su", "ofox", "orangefox", "twrp"]
+__command_list__ = ["magisk", "root", "su", "orangefox", "twrp"]
 __handlers__ = [magisk_handler, orangefox_handler, twrp_handler]
