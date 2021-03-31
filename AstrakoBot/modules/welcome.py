28ca73b17f03e3952b9190fba90e47b80b08aa9c
Astrako <apamarin@gmail.com>
2021-03-31 19:13:06 +0200

    welcome: fix markdown message

--- AstrakoBot/modules/welcome.py
+++ AstrakoBot/modules/welcome.py
@@ -485,9 +485,10 @@ def check_not_bot(member, chat_id, message_id, context):
 
         try:
             bot.edit_message_text(
-                "*kicks user*\nThey can always rejoin and try.",
+                "*Kicks user*\nThey can always rejoin and try.",
                 chat_id=chat_id,
                 message_id=message_id,
+                parse_mode = ParseMode.MARKDOWN,
             )
         except:
             pass
