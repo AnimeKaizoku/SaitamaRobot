ad59b30b6e832d4a549bdf9dd76b210b81e656cd
Abdul Wahid Khan <demolishercrazy@gmail.com>
2021-04-05 19:25:38 +0530

    Updated Replies in a creative way

--- SaitamaRobot/modules/welcome.py
+++ SaitamaRobot/modules/welcome.py
@@ -190,7 +190,7 @@ def new_member(update: Update, context: CallbackContext):
             # Give the owner a special welcome
             if new_mem.id == OWNER_ID:
                 update.effective_message.reply_text(
-                    "Respected {mention}, Welcome To {chatname}.", reply_to_message_id=reply
+                    "My master {first} has joined {chatname}, Show some respect!", reply_to_message_id=reply
                 )
                 welcome_log = (
                     f"{html.escape(chat.title)}\n"
@@ -202,7 +202,7 @@ def new_member(update: Update, context: CallbackContext):
             # Welcome Devs
             elif new_mem.id in DEV_USERS:
                 update.effective_message.reply_text(
-                    "Developer {mention}, Welcome To {chatname}.",
+                    "Oiii!! A member of the Hero Association (Bot Developer) {first} has joined {chatname}",
                     reply_to_message_id=reply,
                 )
                 welcome_log = (
@@ -215,7 +215,7 @@ def new_member(update: Update, context: CallbackContext):
             # Welcome Sudos
             elif new_mem.id in DRAGONS:
                 update.effective_message.reply_text(
-                    "Honoured to have you {mention}, Welcome to {chatname}.",
+                    "{first}, A Dragon admin has joined {chatname}! Bow down",
                     reply_to_message_id=reply,
                 )
                 welcome_log = (
@@ -228,7 +228,7 @@ def new_member(update: Update, context: CallbackContext):
             # Welcome Support
             elif new_mem.id in DEMONS:
                 update.effective_message.reply_text(
-                    "Hey {mention}, Welcome To {chatname}",
+                    "Tch, Deal with Demon member {first} as he has joined {chatname}",
                     reply_to_message_id=reply,
                 )
                 welcome_log = (
@@ -241,7 +241,7 @@ def new_member(update: Update, context: CallbackContext):
             # Welcome Whitelisted
             elif new_mem.id in TIGERS:
                 update.effective_message.reply_text(
-                    "Hello Immortal {mention}, Welcome To {chatname}", reply_to_message_id=reply
+                    "An immortal tiger {first} has joined {chatname}", reply_to_message_id=reply
                 )
                 welcome_log = (
                     f"{html.escape(chat.title)}\n"
@@ -253,7 +253,7 @@ def new_member(update: Update, context: CallbackContext):
             # Welcome Tigers
             elif new_mem.id in WOLVES:
                 update.effective_message.reply_text(
-                    "Hey there {mention}! Welcome To {chatname}", reply_to_message_id=reply
+                    "Behold the furious wolf! {first} has joined {chatname}", reply_to_message_id=reply
                 )
                 welcome_log = (
                     f"{html.escape(chat.title)}\n"
